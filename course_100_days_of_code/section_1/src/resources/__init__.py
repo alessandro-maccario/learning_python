02177b2891de84363a7bed3d9a8e8bc24f5b62aa
alessandro-maccario <a.maccario1@campus.unimib.it>
2023-10-29 11:24:27 +0100

    changing dir structure for learning_python

---
+++ course_100_days_of_code/section_1/src/resources/__init__.py
@@ -0,0 +1,3 @@
+"""
+    In this file, import all the modules and functions/classes present in them to be able to just import them in the main file by referencing to the "resources" folder as "import resources".
+"""
--- materials/README.md
+++
@@ -1,1 +0,0 @@
-This subfolder contains the additional files for each lesson of the 100 days of code challenge, divided by the course number.
