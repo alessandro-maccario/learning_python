461d7e94bc398f0935315eb98057eb317ed0c3ee
alessandro-maccario <a.maccario1@campus.unimib.it>
2023-11-05 15:25:54 +0100

    add variables for narration

--- course_100_days_of_code/section_3/src/main.py
+++ course_100_days_of_code/section_3/src/main.py
@@ -1,5 +1,5 @@
 # import packages
-from pkgs.config import START_TREASURE_ISLAND, INTRO
+from pkgs.config import *
 
 ###################################
 ###### TREASURE ISLAND GAME #######
@@ -10,30 +10,41 @@ if __name__ == "__main__":
     print(START_TREASURE_ISLAND)
 
     # print the introduction story to the game
-    print(INTRO)
+    print(INTRO_DESCRIPTION)
 
     # You should start with a loop so the user can start again whenever they want!
     is_loop_true = True
 
     while is_loop_true:
+        # give the user an intro based on the next decision
+        print(FIRST_LEFT_PATH)
+        print()
+        print(FIRST_RIGHT_PATH)
+
         # let the user choose the first step
-        user_path_decision = input(""" Left or right? """).lower()
+        user_path_decision = input(
+            """ Which one do you chose? Insert left or right? """
+        ).lower()
         print("-----")
 
         # first decision of the user (left = continue adventure; right = game over)
         if user_path_decision == "left":
-            print(""" Your first decision was left. """)
+            print(DEEP_JUNGLE_DECISION)
             print("-----")
 
             # second decision of the user (wait = continue adventure; swim = game over)
-            user_path_decision = input(""" Wait or swim? """).lower()
+            user_path_decision = input(
+                """ Will you proceed to the jungle or to the cliff? Insert jungle or cliff:  """
+            ).lower()
 
-            if user_path_decision == "wait":
-                print(""" Your second decision was wait. """)
+            if user_path_decision == "jungle":
+                print(DEEP_JUNGLE_DECISION)
                 print("-----")
 
-                # third decision of the user
-                user_path_decision = input(""" Which door? """).lower()
+                # third decision of the user: here three
+                user_path_decision = input(
+                    """ Which dore would you like to choose? Insert emerald, sapphire or ruby """
+                ).lower()
                 if user_path_decision == "yellow":
                     print(""" You found the Pirate Treasure! Congrats! """)
                     is_loop_true = False
@@ -48,7 +59,7 @@ if __name__ == "__main__":
                         is_loop_true = False
 
             else:
-                print(""" Game over! """)
+                print(CLIFF_DEFICION)
                 is_continue = input(
                     """ Do you want to start over again?  Insert Y of N: """
                 ).lower()
@@ -58,7 +69,7 @@ if __name__ == "__main__":
                     is_loop_true = False
 
         else:
-            print(""" Game over! """)
+            print(ROCKY_SHORELINE)
             is_continue = input(
                 """ Do you want to start over again?  Insert Y of N: """
             ).lower()
