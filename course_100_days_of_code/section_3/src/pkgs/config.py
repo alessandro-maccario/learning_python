b0183babbb434a15be587b98fa046921ca327c10
alessandro-maccario <a.maccario1@campus.unimib.it>
2023-11-05 12:50:44 +0100

    add config.py file to store all global variables

---
+++ course_100_days_of_code/section_3/src/pkgs/config.py
@@ -0,0 +1,67 @@
+"""
+    This Section 3 of 100 days of code is meant to hold Day 3 Project: Treasure Island.
+
+    References:
+        - For ASCII text art: 
+            https://patorjk.com/software/taag/#p=display&f=Bulbhead&t=The%20Pirate%20Treasure%20Island 
+
+        - For ASCII picture art:
+            https://ascii.co.uk/art
+"""
+
+# main ASCII intro picture
+
+START_TREASURE_ISLAND = '''
+                        
+                        
+
+ ____  _   _  ____    ____  ____  ____    __   ____  ____    ____  ____  ____    __    ___  __  __  ____  ____    ____  ___  __      __    _  _  ____  
+(_  _)( )_( )( ___)  (  _ \(_  _)(  _ \  /__\ (_  _)( ___)  (_  _)(  _ \( ___)  /__\  / __)(  )(  )(  _ \( ___)  (_  _)/ __)(  )    /__\  ( \( )(  _ \ 
+  )(   ) _ (  )__)    )___/ _)(_  )   / /(__)\  )(   )__)     )(   )   / )__)  /(__)\ \__ \ )(__)(  )   / )__)    _)(_ \__ \ )(__  /(__)\  )  (  )(_) )
+ (__) (_) (_)(____)  (__)  (____)(_)\_)(__)(__)(__) (____)   (__) (_)\_)(____)(__)(__)(___/(______)(_)\_)(____)  (____)(___/(____)(__)(__)(_)\_)(____/ 
+
+                        
+                        *******************************************************************************
+                                |                   |                  |                     |
+                        _________|________________.=""_;=.______________|_____________________|_______
+                        |                   |  ,-"_,=""     `"=.|                  |
+                        |___________________|__"=._o`"-._        `"=.______________|___________________
+                                |                `"=._o`"=._      _`"=._                     |
+                        _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+                        |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+                        |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+                                |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+                        _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+                        |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+                        |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+                        ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+                        /______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+                        ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+                        /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+                        ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+                        /______/______/______/______/______/______/______/______/______/______/______/_
+                        *******************************************************************************
+                        '''
+
+INTRO = """
+        Ahoy, matey! 
+        Welcome to the treacherous shores of Skull Island, 
+        a haven for scallywags and for buried treasure. 
+        As you step onto these sun-soaked sands, you become the protagonist of an epic pirate tale. 
+        Your mission is to unravel the mysteries of this legendary island and lay your hands on the 
+        long-lost pirate booty hidden beneath its rugged terrain.
+
+        Once a bustling pirate haven, Skull Island now stands eerily deserted. 
+        The reason? A ghostly curse, they say... 
+        It is whispered in the darkest corners of the seven seas that 
+        the island's treasure is guarded by vengeful spirits, 
+        the restless souls of pirates who perished seeking their ill-gotten gains. 
+        These tormented souls, unable to move on to the afterlife, have driven away all but the boldest of adventurers.
+
+        You'll need to navigate treacherous waters, decipher cryptic maps, 
+        and outsmart cunning pirates who guard the secrets of this island. 
+        The winds of destiny are at your back, and the choice is yours, me heartie. 
+        Will you seize the fortune that awaits, or will you be swallowed by the depths of the sea?
+
+        Are you ready to embark on this swashbuckling quest?"
+        """
