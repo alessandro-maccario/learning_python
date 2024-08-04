c26d67b84ea93e2f0b1903320b211c1f79297401
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-04 11:05:54 +0200

    Add: add operations logic

--- course_100_days_of_code/section_10/calculator/pkgs/operations.py
+++ course_100_days_of_code/section_10/calculator/pkgs/operations.py
@@ -4,17 +4,73 @@ app.py calculator script.
 """
 
 
-def add():
-    pass
+def add(first_number, second_number):
+    """Add two numbers.
+
+    Parameters
+    ----------
+    first_number : _type_
+        _description_
+    second_number : _type_
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    return first_number + second_number
 
 
-def subtract():
-    pass
+def subtract(first_number, second_number):
+    """Subtract two numbers.
+
+    Parameters
+    ----------
+    first_number : _type_
+        _description_
+    second_number : _type_
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    return first_number - second_number
 
 
-def multiply():
-    pass
+def multiply(first_number, second_number):
+    """Multiply two numbers.
+
+    Parameters
+    ----------
+    first_number : _type_
+        _description_
+    second_number : _type_
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    return first_number * second_number
 
 
-def divide():
-    pass
+def divide(first_number, second_number):
+    """Divide two numbers.
+
+    Parameters
+    ----------
+    first_number : _type_
+        _description_
+    second_number : _type_
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    return first_number / second_number
--- course_100_days_of_code/section_10/calculator/src/app.py
+++ course_100_days_of_code/section_10/calculator/src/app.py
@@ -14,7 +14,6 @@ from pkgs.operations import add, subtract, multiply, divide
 print(logo)
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-first_operation = 0
 current_operation = 0
 
 while True:
@@ -24,42 +23,43 @@ while True:
     print("*")
     print("/")
     decision_operation = input("Pick an operation: ")
-    next_number = int(input("What is the next number?"))
+    second_number = int(input("What is the next number?"))
 
     # possible to convert this if-else with a function and reduce the code
     if decision_operation == "+":
-        first_operation += add(
+        current_operation += add(
             first_number,
-            next_number,
+            second_number,
         )
     elif decision_operation == "-":
-        first_operation += subtract(
+        current_operation += subtract(
             first_number,
-            next_number,
+            second_number,
         )
     elif decision_operation == "*":
-        first_operation += multiply(
+        current_operation += multiply(
             first_number,
-            next_number,
+            second_number,
         )
     elif decision_operation == "/":
-        first_operation += divide(
+        current_operation += divide(
             first_number,
-            next_number,
+            second_number,
         )
-
-    # fetch the first value to be taken for following operations, if necessary
-    current_operation = first_operation
 
     decision_continuation = input(
         "Do you want to continue? Type 'y' to continue, type 'n' to start a new calculation, type 'close' to close the application entirely."
     )
 
     if decision_continuation in ("y", "n"):
-        # do something in a function
-        pass
+        if decision_continuation == "y":
+            continue
+        else:
+            current_operation = 0
+            break
     elif decision_continuation == "close":
         # close the application
+        print("Closing the calculator...")
         break
     else:
         print(
