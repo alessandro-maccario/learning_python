9909a6fd7b118362896ae52141d08a5c24428c64
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-08 07:23:29 +0200

    Refactor: remove unuseful code

--- course_100_days_of_code/section_10/calculator/src/app.py
+++ course_100_days_of_code/section_10/calculator/src/app.py
@@ -9,17 +9,16 @@ import sys
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pkgs.ascii_art import logo
-from pkgs.operations import add, subtract, multiply, divide, calculator
+from pkgs.operations import calculator
 
 print(logo)
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-current_operation = 0
+# keep track of the previous value
 history_value = 0
-count_running = 0
 
-operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 first_number = float(input("What is the first number?: "))
 
 if __name__ == "__main__":
+    # run the function
     calculator(first_number, history_value)
