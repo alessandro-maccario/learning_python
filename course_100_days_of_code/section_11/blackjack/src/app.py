918b65d8cc19a625c9753feeae20336bec690e12
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-10 12:33:09 +0200

    Fix: fix the game logic

--- course_100_days_of_code/section_11/blackjack/src/app.py
+++ course_100_days_of_code/section_11/blackjack/src/app.py
@@ -5,17 +5,30 @@ This script will create a simple Blackjack game in Python for the 100 Days of Co
 # --- IMPORT PACKAGES --- #
 import os
 import sys
+from copy import deepcopy
 from random import sample
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from pkgs.helper import append_new_card, reset_card_list, clear_screen
+from pkgs.helper import (
+    append_new_card,
+    reset_card_list,
+    player_lose,
+    clear_screen,
+    stop_game,
+    DEALER_WINS,
+    PLAYER_WINS,
+    RESTART_APPLICATION,
+    HIT_OR_STAY,
+    DRAW,
+)
 
 # possible card choices
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # reset the card lists
 dealer_cards, user_cards = reset_card_list()
-
+number_of_player_wins = 0
+number_of_games = 0
 
 while True:
     if len(dealer_cards) == 0:
@@ -28,64 +41,75 @@ while True:
         sum_dealer_cards = sum(dealer_cards)
         sum_user_cards = sum(user_cards)
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+        # make a copy of the dealer cards and sum up values using this list
+        original_dealer_cards = deepcopy(dealer_cards)
+        hide_dealer_cards = deepcopy(original_dealer_cards)
+        # hide the first element of the dealer cards
+        hide_dealer_cards[0] = "*"
+
+        print("Dealer's cards: ", hide_dealer_cards, f"= {dealer_cards[1]}")
         print("User's cards: ", user_cards, f"= {sum_user_cards}")
 
-        if sum_dealer_cards == 21:
-            print("##### DEALER WINS, PLAYER BUSTED! #####")
-            # reset the card lists
-            dealer_cards, user_cards = reset_card_list()
-            continue
-        elif sum_user_cards == sum_dealer_cards:
-            print("##### DRAW #####")
-            print("Restarting the game...")
-            # reset the card lists
-            dealer_cards, user_cards = reset_card_list()
-            continue
-
-        # user's card calculation
         if sum_user_cards > 21:
-            print("The sum of your cards is: ", sum_user_cards)
-            print("##### BUSTED #####")
-            decision = input(
-                "Do you want to play another game? Insert 'y' to start a new game, insert 'n' to close the application."
-            )
+            decision = player_lose(sum_user_cards)
             if decision == "y":
+                number_of_games += 1
                 clear_screen()
+                # reset the card lists
+                dealer_cards, user_cards = reset_card_list()
                 continue
             else:
+                stop_game(number_of_player_wins, number_of_games)
                 break
         elif sum_user_cards < 21:
-            decision_more_card = input("HIT or STAY? Insert 'y' or 's': ")
+            decision_more_card = input(HIT_OR_STAY)
             if decision_more_card == "y":
                 print("#############################")
-                # dealer_cards = append_new_card(cards, dealer_cards)
                 user_cards = append_new_card(cards, user_cards)
 
                 sum_dealer_cards = sum(dealer_cards)
                 sum_user_cards = sum(user_cards)
 
-                print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                print("Dealer's cards: ", hide_dealer_cards, f"= {dealer_cards[1]}")
                 print("User's cards: ", user_cards, f"= {sum_user_cards}")
 
                 if sum_user_cards > 21:
-                    print("##### PLAYER GOT BUSTED #####")
-                    decision = input(
-                        "Do you want to play another game? Insert 'y' to start a new game, insert 'n' to close the application: "
-                    )
+                    decision = player_lose(sum_user_cards)
                     if decision == "y":
+                        number_of_games += 1
                         clear_screen()
                         # reset the card lists
                         dealer_cards, user_cards = reset_card_list()
                         continue
                     else:
-                        print("Thanks for playing with us! Come back soon!")
+                        stop_game(number_of_player_wins, number_of_games)
                         break
 
             # the user chose to 'stay': the dealer will get more cards until
             # they win, draw or lose
             else:
                 while True:
+                    if sum_dealer_cards == 21:
+                        print(DEALER_WINS)
+                        print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                        # reset the card lists
+                        dealer_cards, user_cards = reset_card_list()
+                        break
+                    elif sum_user_cards == sum_dealer_cards:
+                        print(DRAW)
+                        print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                        number_of_games += 1
+                        # reset the card lists
+                        dealer_cards, user_cards = reset_card_list()
+                        break
+                    elif (sum_dealer_cards > sum_user_cards) and sum_dealer_cards < 21:
+                        print(DEALER_WINS)
+                        print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                        number_of_games += 1
+                        # reset the card lists
+                        dealer_cards, user_cards = reset_card_list()
+                        break
+
                     dealer_cards = append_new_card(cards, dealer_cards)
                     # sum the card numbers
                     sum_dealer_cards = sum(dealer_cards)
@@ -94,55 +118,59 @@ while True:
                     print("User's cards: ", user_cards, f"= {sum_user_cards}")
 
                     if sum_dealer_cards == 21:
-                        print("##### DEALER WINS, PLAYER BUSTED! #####")
+                        print(DEALER_WINS)
+                        print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
                         # reset the card lists
                         dealer_cards, user_cards = reset_card_list()
                         break
                     elif (sum_dealer_cards > 21) and (sum_user_cards < 21):
-                        print("##### PLAYER WINS, DEALER BUSTED! #####")
+                        print(PLAYER_WINS)
+                        number_of_player_wins += 1
                         # reset the card lists
                         dealer_cards, user_cards = reset_card_list()
                         break
 
         elif (sum_user_cards == 21) and (sum_user_cards > sum_dealer_cards):
-            print("##### PLAYER WINS, DEALER BUSTED! #####")
+            print(PLAYER_WINS)
+            number_of_player_wins += 1
+            number_of_games += 1
             # reset the card lists
             dealer_cards, user_cards = reset_card_list()
             continue
 
     else:
         if sum_dealer_cards == 21:
-            print("##### DEALER WINS, PLAYER BUSTED! #####")
+            print(DEALER_WINS)
             # reset the card lists
             dealer_cards, user_cards = reset_card_list()
             continue
         elif (sum_dealer_cards > 21) and (sum_user_cards < 21):
-            print("##### PLAYER WINS, DEALER BUSTED! #####")
-            decision = input(
-                "Do you want to play another game? Insert 'y' to start a new game, insert 'n' to close the application."
-            )
+            print(PLAYER_WINS)
+            number_of_player_wins += 1
+            decision = input(RESTART_APPLICATION)
             if decision == "y":
+                number_of_games += 1
                 clear_screen()
                 # reset the card lists
                 dealer_cards, user_cards = reset_card_list()
                 continue
             else:
-                print("Thanks for playing with us! Come back soon!")
+                number_of_games += 1
+                stop_game(number_of_player_wins, number_of_games)
                 break
 
         # user's card calculation
         if sum_user_cards == 21:
-            print("##### PLAYER WINS, DEALER BUSTED! #####")
+            print(PLAYER_WINS)
+            number_of_player_wins += 1
+            number_of_games += 1
             # reset the card lists
             dealer_cards, user_cards = reset_card_list()
             continue
         elif sum_user_cards > 21:
-            print("The sum of your cards is: ", sum_user_cards)
-            print("##### PLAYER BUSTED #####")
-            decision = input(
-                "Do you want to play another game? Insert 'y' to start a new game, insert 'n' to close the application."
-            )
+            decision = player_lose(sum_user_cards)
             if decision == "y":
+                number_of_games += 1
                 clear_screen()
                 # reset the card lists
                 dealer_cards, user_cards = reset_card_list()
@@ -150,42 +178,59 @@ while True:
             else:
                 break
         elif sum_user_cards < 21:
-            decision_more_card = input("HIT or STAY? Insert 'y' or 's': ")
+            decision_more_card = input(HIT_OR_STAY)
             if decision_more_card == "y":
                 print("#############################")
-                # dealer_cards = append_new_card(cards, dealer_cards)
                 user_cards = append_new_card(cards, user_cards)
 
                 # sum the card numbers
                 sum_dealer_cards = sum(dealer_cards)
                 sum_user_cards = sum(user_cards)
 
-                print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                print("Dealer's cards: ", hide_dealer_cards, f"= {dealer_cards[1]}")
                 print("User's cards: ", user_cards, f"= {sum_user_cards}")
 
                 if sum_user_cards == sum_dealer_cards:
-                    print("##### DRAW #####")
-                    print("Restarting the game...")
+                    print(DRAW)
+                    print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                    number_of_games += 1
                     # reset the card lists
                     dealer_cards, user_cards = reset_card_list()
                     continue
 
                 elif sum_user_cards > 21:
-                    print("##### PLAYER BUSTED #####")
-                    decision = input(
-                        "Another game? Insert 'y' to start a new game, insert 'n' to close the application: "
-                    )
+                    print(DEALER_WINS)
+                    print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                    decision = input(RESTART_APPLICATION)
                     if decision == "y":
+                        number_of_games += 1
                         clear_screen()
                         # reset the card lists
                         dealer_cards, user_cards = reset_card_list()
                         continue
                     else:
+                        number_of_games += 1
+                        stop_game(number_of_player_wins, number_of_games)
                         break
             # the user chose to 'stay': the dealer will get more cards until
             # they win, draw or lose
             else:
                 while True:
+                    if sum_dealer_cards == 21:
+                        print(DEALER_WINS)
+                        print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                        number_of_games += 1
+                        # reset the card lists
+                        dealer_cards, user_cards = reset_card_list()
+                        break
+                    elif sum_user_cards == sum_dealer_cards:
+                        print(DRAW)
+                        print("Dealer's cards: ", dealer_cards, f"= {sum_dealer_cards}")
+                        number_of_games += 1
+                        # reset the card lists
+                        dealer_cards, user_cards = reset_card_list()
+                        break
+
                     dealer_cards = append_new_card(cards, dealer_cards)
                     # sum the card numbers
                     sum_dealer_cards = sum(dealer_cards)
@@ -194,31 +239,31 @@ while True:
                     print("User's cards: ", user_cards, f"= {sum_user_cards}")
 
                     if sum_dealer_cards == 21:
-                        print("##### DEALER WINS, PLAYER BUSTED! #####")
+                        print(DEALER_WINS)
+                        number_of_games += 1
+                        # reset the card lists
+                        dealer_cards, user_cards = reset_card_list()
+                        break
+                    elif sum_dealer_cards > sum_user_cards:
+                        print(DEALER_WINS)
+                        number_of_games += 1
                         # reset the card lists
                         dealer_cards, user_cards = reset_card_list()
                         break
                     elif (sum_dealer_cards > 21) and (sum_user_cards < 21):
-                        print("##### PLAYER WINS, DEALER BUSTED! #####")
+                        print(PLAYER_WINS)
+                        number_of_player_wins += 1
                         # reset the card lists
                         dealer_cards, user_cards = reset_card_list()
                         break
-                    # elif (sum_user_cards < 22) and (sum_user_cards > sum_dealer_cards):
-                    #     print("##### PLAYER WINS, DEALER BUSTED! #####")
-                    #     # reset the card lists
-                    #     dealer_cards, user_cards = reset_card_list()
-                    #     break
 
         elif (sum_user_cards < 22) and (sum_user_cards > sum_dealer_cards):
-            print("##### PLAYER WINS, DEALER BUSTED! #####")
+            print(PLAYER_WINS)
+            number_of_player_wins += 1
+            number_of_games += 1
             # reset the card lists
             dealer_cards, user_cards = reset_card_list()
             continue
-
-# TODO:
-# Missing the 'stay' option:
-# in case the user selects 'stay', then only the dealer should get more card until they BUST
-# or they WIN.
 
 # TODO:
 # Convert repeated code into functions
