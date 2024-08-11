93c769139afa398b4fefd4c9ffded7ae59d5638a
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-11 15:24:41 +0200

    Refactor: change folder names from Section_* to day_*

---
+++ course_100_days_of_code/day_12/guess_the_number/src/app.py
@@ -0,0 +1,13 @@
+"""
+This script will create a simple Guess the Number game in Python for the 100 Days of Code challenge from the Udemy course.
+"""
+
+# --- IMPORT PACKAGES --- #
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+# --- MAIN CODE --- #
