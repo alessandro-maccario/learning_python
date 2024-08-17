9a39e715ed958d579a5e06d78460d69f4b4b6ed2
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-17 20:01:31 +0200

    Refactor: refactor the game play to use iterators instead of splitting game play between first and subsequent turns

--- course_100_days_of_code/day_14/higher_lower_game/src/app.py
+++ course_100_days_of_code/day_14/higher_lower_game/src/app.py
@@ -8,114 +8,76 @@ Reference:
 # --- IMPORT PACKAGES --- #
 import os
 import sys
-from random import choice
-from copy import deepcopy
+from random import shuffle
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from pkgs.ascii_art import main_logo, vs_logo
+from pkgs.ascii_art import main_logo
 from data.data import data
-from pkgs.game_logic import compare_number_followers, print_comparison
-
-# --- VARIABLES --- #
-count_correct_answer = 0
-previous_option = None
-
+from pkgs.game_logic import (
+    compare_number_followers,
+    print_comparison,
+    end_game_comment,
+    input_decision,
+)
 
 # --- MAIN CODE --- #
 print(main_logo)
 
+# --- VARIABLES --- #
+score = 0
+previous_option = None
 
-original_data = deepcopy(data)
-already_seen = []
 
-while True:
-    if previous_option is None:
-        # COMPARISON A
-        # randomly choose the first element
-        comparison_a = choice(data)
-        # get the corresponding index. Needed to avoid picking the same element twice
-        idx_comparison_a = original_data.index(comparison_a)
+def main():
+    global previous_option, score  # get the variable from the global scope
 
-        # COMPARISON B
-        # do not pick the same element as comparison_a for comparison_b
-        # data = [x for x in data if data.index(x) != idx_comparison_a]
-        already_seen.append(data.pop(idx_comparison_a))
+    # shuffle in place
+    shuffle(data)
+    data_iterator = iter(data)  # Create an iterator for the shuffled data
 
-        # randomly choose the second element
-        comparison_b = choice(data)
-        # get the corresponding index. Needed to avoid picking the same element twice.
-        # # Need to get the index from the original list
-        # idx_comparison_b = original_data.index(comparison_b)
+    while True:
+        if previous_option is None:
+            comparison_a = next(data_iterator)
+        else:
+            comparison_a = previous_option
+            previous_option = None
+
+        try:
+            comparison_b = next(
+                data_iterator
+            )  # get the next element for the comparison with comparison_a
+        except StopIteration:
+            # print statement if the user is above or below score
+            end_game_comment(score)
+            break
 
         # print the comparison between the first and the second element
         print_comparison(comparison_a, comparison_b)
 
         # let the player decides which one between A and B is correct
-        decision = input("Who has more followers? Type 'A' or 'B': ")
+        decision = input_decision()
+
         print()
 
-        # if compare_number_followers is None, then give previous_option count_correct_answer a
+        # if compare_number_followers is None, then give previous_option score a
         # None value
         try:
-            previous_option, count_correct_answer = compare_number_followers(
+            previous_option, score = compare_number_followers(
                 decision,
                 comparison_a,
                 comparison_b,
-                count_correct_answer,
+                score,
                 previous_option,
             )
         except TypeError:
             previous_option = None
 
-        if previous_option is not None:
-            continue
-        else:
-            break
-    else:
-        # if we are not in the first turn, comparison_a will be the previous
-        comparison_a = previous_option
-
-        # COMPARISON B
-        # do not pick the same element as comparison_a for comparison_b
-        # data = [x for x in data if data.index(previous_option) != idx_comparison_a]
-
-        previous_option = None
-        # randomly choose the second element
-        comparison_b = None
-        comparison_b = choice(data)
-        # # get the corresponding index. Needed to avoid picking the same element twice
-        idx_comparison_b = data.index(comparison_b)
-        already_seen.append(data.pop(idx_comparison_b))
-
-        # print the comparison between the first and the second element
-        print_comparison(comparison_a, comparison_b)
-
-        # let the player decides which one between A and B is correct
-        decision = input("Who has more followers? Type 'A' or 'B': ")
-        print()
-
-        # if compare_number_followers is None, then give previous_option count_correct_answer a
-        # None value
-        try:
-            previous_option, count_correct_answer = compare_number_followers(
-                decision,
-                comparison_a,
-                comparison_b,
-                count_correct_answer,
-                previous_option,
-            )
-        except TypeError:
-            previous_option = None
-        if previous_option is not None:
-            continue
-        else:
+        if previous_option is None:
             break
 
 
-if count_correct_answer < 3:
-    print(f"Too bad, try again! Highest score: {count_correct_answer}")
-else:
-    print(
-        f"Above the average of 3 correct answer! Good job! Highest score: {count_correct_answer}"
-    )
+if __name__ == "__main__":
+    main()
+    # print statement if the user is above or below score
+    end_game_comment(score)
