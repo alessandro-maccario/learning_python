4da29f2a71b38895fe7e48bd453a93a539ba3467
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-18 11:17:01 +0200

    Initial commit: first commit for the coffee machine project

---
+++ course_100_days_of_code/day_15/project_coffee_machine/src/app.py
@@ -0,0 +1,15 @@
+"""
+This script will create ??? in Python for the 100 Days of Code challenge from the Udemy course.
+
+"""
+
+# --- IMPORT PACKAGES --- #
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from pkgs.ascii_art import main_logo
+
+
+# --- MAIN CODE --- #
