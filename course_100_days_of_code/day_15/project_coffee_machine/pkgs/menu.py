509f7a8b83f7471d260f413c6873d91874f99e2d
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-18 19:55:17 +0200

    Add: add resources and the menu

---
+++ course_100_days_of_code/day_15/project_coffee_machine/pkgs/menu.py
@@ -0,0 +1,12 @@
+# --- CONSTANTS --- #
+MENU = {
+    "espresso": {
+        "Ingredients": {"water": 50, "coffee": 18},
+        "cost": 1.5,
+    },
+    "latte": {"Ingredients": {"water": 50, "milk": 150, "coffee": 24}, "cost": 2.5},
+    "cappuccino": {
+        "Ingredients": {"water": 250, "milk": 100, "coffee": 24},
+        "cost": 3.0,
+    },
+}
---
+++ course_100_days_of_code/day_15/project_coffee_machine/pkgs/resources.py
@@ -0,0 +1,2 @@
+# Coffee machine initial resources
+resources = {"water": 300, "milk": 200, "coffee": 100}
