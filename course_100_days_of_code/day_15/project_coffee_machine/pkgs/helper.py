89227759ec4554ebc902fc2ccbfae92f0ada8bc2
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-21 21:14:59 +0200

    Add: add user_input and give_change functions

--- course_100_days_of_code/day_15/project_coffee_machine/pkgs/helper.py
+++ course_100_days_of_code/day_15/project_coffee_machine/pkgs/helper.py
@@ -2,19 +2,41 @@
 Script to keep the helper functions that are not game logic related.
 """
 
-# --- IMPORT PACKAGES --- #
-import os
-import sys
 
-# dynamically adjust the PYTHONPATH
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from pkgs.resources import resources
+def user_input():
+    """Ask for the user's input"""
+    decision = input("What would you like? Type Espresso/Latte/Cappuccino: ").lower()
+    while decision not in ("espresso", "latte", "cappuccino", "report", "off"):
+        print("Please, enter a valid option between espresso/latte/cappuccino.")
+        decision = input(
+            "What would you like? Type Espresso/Latte/Cappuccino: "
+        ).lower()
+    return decision
 
 
-def display_report(money: float) -> None:
-    """Generating a report of the available resources"""
-    print("Water:", resources["water"])
-    print("Milk:", resources["milk"])
-    print("Coffee:", resources["coffee"])
-    print("Profit: $", money)
+def display_report(profit: float, resources: dict) -> None:
+    """Generating a report of the available resources.
+
+    Parameters
+    ----------
+    profit : float
+        The total amount of money collected by the coffee machine
+    resources : dict
+        Current resources available in the coffee machine
+    """
+    print("> 💧 Water:", resources["water"])
+    print("> 🥛 Milk:", resources["milk"])
+    print("> ☕ Coffee:", resources["coffee"])
+    print("> 💲 Profit: $", profit)
     return
+
+
+def prepare_drink(decision):
+    """If the resources are enough, then prepare the drink."""
+    print(f">>> Start pouring {decision} ☕... Enjoy 😄 !")
+    return
+
+
+def give_change(payment_process):
+    """Print a statement when you give money back to the user"""
+    print(f">>> Here is ${payment_process} dollars in change!")
