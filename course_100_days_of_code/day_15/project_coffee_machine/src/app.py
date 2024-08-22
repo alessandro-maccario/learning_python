ea32e71c4ae2f0c308f4ba47546129b614b65b18
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-22 07:31:54 +0200

    Add: add add_profit function to the main code

--- course_100_days_of_code/day_15/project_coffee_machine/src/app.py
+++ course_100_days_of_code/day_15/project_coffee_machine/src/app.py
@@ -9,23 +9,26 @@ import sys
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pkgs.coffee_machine_logic import coffee_machine_working_logic
-from pkgs.helper import display_report, user_input
+from pkgs.helper import display_report, user_input, add_profit
 from pkgs.resources import resources
+from pkgs.menu import MENU
 
 
 # --- VARIABLES --- #
 profit = 0
+machine_profit = 0
 decision = None
 current_resource_availability = resources
 
 
 # --- MAIN CODE --- #
 def main():
-    global profit, decision, current_resource_availability  # get the global variables
+    global profit, decision, current_resource_availability, machine_profit  # get the global variables
 
     while decision != "off":
         print()
-        decision = user_input()
+        decision = user_input()  # collect the user's input
+
         if decision == "espresso":
             # call the coffee machine logic
             logic = coffee_machine_working_logic(
@@ -33,6 +36,7 @@ def main():
             )
             if logic is False:
                 break
+            machine_profit += add_profit(decision, profit, MENU)
 
         elif decision == "latte":
             # call the coffee machine logic
@@ -41,6 +45,7 @@ def main():
             )
             if logic is False:
                 break
+            machine_profit += add_profit(decision, profit, MENU)
 
         elif decision == "cappuccino":
             # call the coffee machine logic
@@ -49,13 +54,14 @@ def main():
             )
             if logic is False:
                 break
+            machine_profit += add_profit(decision, profit, MENU)
 
         elif decision == "off":
             sys.exit("Turning off the machine...")
 
         else:  # the decision is report
             display_report(
-                profit, current_resource_availability
+                machine_profit, current_resource_availability
             )  # show the available resources
 
 
