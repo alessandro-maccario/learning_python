3863c917ee5e19626ea4a1fb2316794a323524c0
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-28 07:40:05 +0200

    Add: add while loop to loop through the questions and show them to the player

--- course_100_days_of_code/day_17/quiz_game/src/app.py
+++ course_100_days_of_code/day_17/quiz_game/src/app.py
@@ -2,3 +2,29 @@
 This script will create a simple Quiz Game in Python for the 100 Days of Code challenge from the Udemy course.
 
 """
+
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pkgs.question_model import Question
+from data.data import question_data
+from pkgs.quiz_brain import QuizBrain
+
+
+question_bank = []
+
+for idx, question_and_answer in enumerate(question_data):
+    question_bank.append(
+        Question(question_data[idx]["text"], question_data[idx]["answer"])
+    )
+
+# print(question_bank)
+
+quiz = QuizBrain(question_bank)
+
+print("#########################")
+print("#########################")
+while quiz.still_has_question():
+    quiz.next_question()
