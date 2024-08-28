b5acf26078496c29b0ca5af20971e32cd1fac4e0
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-28 18:07:45 +0200

    Refactor: use of the new TriviaDB CS questions

--- course_100_days_of_code/day_17/quiz_game/src/app.py
+++ course_100_days_of_code/day_17/quiz_game/src/app.py
@@ -9,27 +9,36 @@ import sys
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pkgs.question_model import Question
-from data.data import question_data
+
+# from data.data import question_data # old data
+from data.data_triviadb import question_data  # data from TriviaDB
 from pkgs.quiz_brain import QuizBrain
 
-
+# create a list to hold all the questions
 question_bank = []
 
+# Append each question to the question_bank list as a Question() object
 for idx, question_and_answer in enumerate(question_data):
     question_bank.append(
-        Question(question_data[idx]["text"], question_data[idx]["answer"])
+        Question(question_data[idx]["question"], question_data[idx]["correct_answer"])
     )
 
-# print(question_bank)
-
+# instiate the QuizBrain class with the question_bank
 quiz = QuizBrain(question_bank)
 
-#
-while quiz.still_has_question():
-    question = quiz.next_question()
 
-# End of the game
-print("You have completed this challenge!")
-print(
-    f"Your final score is: {quiz.score}/ {len(quiz.question_list)} = {round(quiz.score/len(quiz.question_list), 2)*100}"
-)
+def main():
+    global question  # make the question variable available globally
+    while quiz.still_has_question():
+        question = quiz.next_question()
+
+    # End of the game
+    print("You have completed this challenge!")
+    print(
+        f"Your final score is: {quiz.score}/ {len(quiz.question_list)} = {round(quiz.score/len(quiz.question_list), 2)*100}"
+    )
+
+
+# --- MAIN --- #
+if __name__ == "__main__":
+    main()
