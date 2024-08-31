0ffd90f4e82bef554596a76eb7ef068b45581998
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-08-31 08:51:17 +0200

    Add: instantiate the shape object and draw a square

--- course_100_days_of_code/day_18/turtle_art/src/app.py
+++ course_100_days_of_code/day_18/turtle_art/src/app.py
@@ -1,1 +1,32 @@
-""" """
+"""
+This script will create a simple Turtle Art Paint in Python for the 100 Days of Code challenge from the Udemy course.
+
+"""
+
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# --- IMPORT PACKAGES --- #
+from turtle import Turtle, Screen
+from pkgs.move import Shape
+
+# instanciate the turtle
+turtle = Turtle()
+
+# define the characteristics of the turtle
+turtle.shape("circle")
+turtle.color("green")
+
+# instantiate the shape object
+shape = Shape()
+# --- Draw a square --- #
+shape.square(turtle=turtle)
+
+# Define the screen where the turtle will draw
+# instantiate the screen where the turtle will play
+screen = Screen()
+# once the user clicks on the window, the window will close automatically
+screen.exitonclick()
