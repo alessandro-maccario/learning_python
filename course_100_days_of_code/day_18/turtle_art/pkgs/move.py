9c3bfe315580644e73d4371acd9ec95eedefe294
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-02 06:41:20 +0200

    Add: add def dashed_line()

--- course_100_days_of_code/day_18/turtle_art/pkgs/move.py
+++ course_100_days_of_code/day_18/turtle_art/pkgs/move.py
@@ -8,12 +8,33 @@ class Shape:
         Parameters
         ----------
         turtle : object
-            The object used to draw.
+            The turtle object used to draw.
         """
         turtle.home()  # Move turtle to the origin – coordinates (0,0)
         # --- Draw a square --- #
-        for turn in range(0, 4):
+        """
+        The underscore is a convention meaning "doesn't matter what this is". 
+        So the loop will iterate n times, but without making use of a classic iteration counter. 
+        Technically the iteration counter does still exist, 
+        but if you're going to actually USE it, you'd rename it to something else
+        """
+        for _ in range(0, 4):
             turtle.forward(50)
             turtle.left(90)
 
+        return
+
+    def dashed_line(self, turtle: object) -> None:
+        """Use the turtle to draw a dashed line.
+
+        Parameters
+        ----------
+        turtle : object
+            The turtle object used to draw.
+        """
+        for _ in range(0, 10):
+            turtle.forward(10)
+            turtle.up()
+            turtle.forward(5)
+            turtle.down()
         return
