5415fb235f16e020cda35926c087e006427272ac
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-02 22:52:09 +0200

    Refactor: create one method to rule them all

--- course_100_days_of_code/day_18/turtle_art/pkgs/move.py
+++ course_100_days_of_code/day_18/turtle_art/pkgs/move.py
@@ -11,164 +11,55 @@ class Shape:
         """Generate a random color in hex format."""
         return "#%06x" % randint(0, 0xFFFFFF)
 
-    def square(self, turtle: object, turtle_color: str = None) -> None:
-        """Draw a square using a turtle object
+    def geometric_shape(
+        self, turtle: object, number_of_sides: int, turtle_color: str = None
+    ) -> None:
+        """Draw a geometric shape (triangle, square, ...) using a turtle object and
+        by defining the number of sides that the shape must have.
+
+        Possible shapes:
+        - triangle (number of sides: 3)
+        - square (number of sides: 4)
+        - pentagone (number of sides: 5)
+        - hexagone (number of sides: 6)
+        - eptagone (number of sides: 7)
+        - octagon (number of sides: 8)
+        - nonagon (number of sides: 9)
+        - decagon (number of sides: 10)
 
         Parameters
         ----------
         turtle : object
             The turtle object used to draw.
+        number_of_sides : int
+            Number of sides of the shape
         turtle_color: str
             The turtle color object used to draw.
         """
-        turtle.home()  # Move turtle to the origin – coordinates (0,0)
-        # define a color for the turtle
-        turtle_color = self.random_color()
-        turtle.color(turtle_color)
-        # --- Draw a square --- #
-        """
-        The underscore is a convention meaning "doesn't matter what this is". 
-        So the loop will iterate n times, but without making use of a classic iteration counter. 
-        Technically the iteration counter does still exist, 
-        but if you're going to actually USE it, you'd rename it to something else
-        """
-        for _ in range(0, 4):
-            turtle.forward(self.side_length)
-            turtle.left(90)
-
-        return
-
-    def triangle(self, turtle: object, turtle_color: str = None) -> None:
-        """Use the turtle object to draw a triangle
-
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
+        turtle.setheading(0)  # Move turtle to the origin – coordinates (0,0)
         # define a color for the turtle
         turtle_color = self.random_color()
         turtle.color(turtle_color)
 
-        for _ in range(0, 3):
+        for _ in range(0, number_of_sides):
             turtle.forward(self.side_length)
-            turtle.left(120)
+            turtle.left(360 / number_of_sides)
+
         return
 
-    def pentagon(self, turtle: object, turtle_color: str = None) -> None:
-        """Use the turtle object to draw a pentagon
 
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
-        # define a color for the turtle
-        turtle_color = self.random_color()
-        turtle.color(turtle_color)
+def dashed_line(turtle: object) -> None:
+    """Use the turtle to draw a dashed line.
 
-        for _ in range(0, 5):
-            turtle.forward(self.side_length)
-            turtle.left(72)
-        return
+    Parameters
+    ----------
+    turtle : object
+        The turtle object used to draw.
+    """
 
-    def hexagon(self, turtle: object, turtle_color=None) -> None:
-        """Use the turtle object to draw a pentagon
-
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
-        # define a color for the turtle
-        turtle_color = self.random_color()
-        turtle.color(turtle_color)
-
-        for _ in range(0, 6):
-            turtle.forward(self.side_length)
-            turtle.left(60)
-        return
-
-    def heptagon(self, turtle: object, turtle_color: str = None) -> None:
-        """Use the turtle object to draw a pentagon
-
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
-        # define a color for the turtle
-        turtle_color = self.random_color()
-        turtle.color(turtle_color)
-
-        for _ in range(0, 7):
-            turtle.forward(self.side_length)
-            turtle.left(51.43)
-        return
-
-    def octagone(self, turtle: object, turtle_color: str = None) -> None:
-        """Use the turtle object to draw a pentagon
-
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
-        # define a color for the turtle
-        turtle_color = self.random_color()
-        turtle.color(turtle_color)
-
-        for _ in range(0, 8):
-            turtle.forward(self.side_length)
-            turtle.left(45)
-        return
-
-    def nonagone(self, turtle: object, turtle_color: str = None) -> None:
-        """Use the turtle object to draw a pentagon
-
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
-        # define a color for the turtle
-        turtle_color = self.random_color()
-        turtle.color(turtle_color)
-
-        for _ in range(0, 9):
-            turtle.forward(self.side_length)
-            turtle.left(40)
-        return
-
-    def decagone(self, turtle: object, turtle_color: str = None) -> None:
-        """Use the turtle object to draw a pentagon
-
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
-        # define a color for the turtle
-        turtle_color = self.random_color()
-        turtle.color(turtle_color)
-
-        for _ in range(0, 10):
-            turtle.forward(self.side_length)
-            turtle.left(36)
-        return
-
-    def dashed_line(self, turtle: object) -> None:
-        """Use the turtle to draw a dashed line.
-
-        Parameters
-        ----------
-        turtle : object
-            The turtle object used to draw.
-        """
-
-        for _ in range(0, 10):
-            turtle.forward(10)
-            turtle.up()
-            turtle.forward(5)
-            turtle.down()
-        return
+    for _ in range(0, 10):
+        turtle.forward(10)
+        turtle.up()
+        turtle.forward(5)
+        turtle.down()
+    return
