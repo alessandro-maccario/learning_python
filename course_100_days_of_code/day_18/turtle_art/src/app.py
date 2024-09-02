f3d9fc47d54e34cbbd102f6e8c65fab58e631887
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-02 18:32:20 +0200

    Add: add code to call each shape with a different color

--- course_100_days_of_code/day_18/turtle_art/src/app.py
+++ course_100_days_of_code/day_18/turtle_art/src/app.py
@@ -5,6 +5,7 @@ This script will create a simple Turtle Art Paint in Python for the 100 Days of 
 
 import os
 import sys
+from random import randint
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -18,19 +19,42 @@ turtle = Turtle()
 
 # define the characteristics of the turtle
 # turtle.shape("circle")
-turtle.color("green")
+# turtle.color(randint(0, 255))
 
 # --- SHAPES --- #
 # instantiate the shape object
 shape = Shape()
+
 # --- Draw a square --- #
 shape.square(turtle=turtle)
-
 # erase the screen
-turtle.clear()
+# turtle.clear()
+
+# --- Draw a triangle --- #
+shape.triangle(turtle=turtle)
+# erase the screen
+# turtle.clear()
+
+# --- Draw a pentagon --- #
+shape.pentagon(turtle=turtle)
+
+# --- Draw a hexagon --- #
+shape.hexagon(turtle=turtle)
+
+# --- Draw a heptagon --- #
+shape.heptagon(turtle=turtle)
+
+# --- Draw a octagone --- #
+shape.octagone(turtle=turtle)
+
+# --- Draw a nonagone --- #
+shape.nonagone(turtle=turtle)
+
+# --- Draw a decagone --- #
+shape.decagone(turtle=turtle)
 
 # --- LINES --- #
-shape.dashed_line(turtle=turtle)
+# shape.dashed_line(turtle=turtle)
 
 # --- Screen and Exit --- #
 # Define the screen where the turtle will draw
