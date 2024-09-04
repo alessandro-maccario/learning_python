4d6dad2d0233584c80ed547ca0f01dac15b029a8
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-04 19:50:56 +0200

    Refactor: make the turtle invisible

--- course_100_days_of_code/day_18/turtle_art/src/app.py
+++ course_100_days_of_code/day_18/turtle_art/src/app.py
@@ -14,21 +14,23 @@ from turtle import Turtle, Screen
 from pkgs.move import Shape, random_walk
 
 # instanciate the turtle
-turtle = Turtle()
+turtle = Turtle(visible=False)
 
 # define the characteristics of the turtle
 # turtle.shape("circle")
 # turtle.color(randint(0, 255))
 
-# # --- SHAPES --- #
-# # instantiate the shape object
-# shape = Shape()
+# --- SHAPES --- #
+# instantiate the shape object
+shape = Shape()
 
+# Draw all the shapes based on the number of sides input
 # for i in range(3, 11):
 #     shape.geometric_shape(turtle=turtle, number_of_sides=i)
 
 # --- Define a random walk for the turtle --- #
-random_walk(turtle=turtle, number_of_steps=30)
+# random_walk(turtle=turtle, number_of_steps=200)
+shape.spirograph(turtle=turtle, radius=50, angle_head=10)
 
 # --- LINES --- #
 # shape.dashed_line(turtle=turtle)
@@ -37,5 +39,6 @@ random_walk(turtle=turtle, number_of_steps=30)
 # Define the screen where the turtle will draw
 # instantiate the screen where the turtle will play
 screen = Screen()
+screen.colormode(255)
 # once the user clicks on the window, the window will close automatically
 screen.exitonclick()
