e0f3f7bc3221c68e52327632c71dc46b2abc9164
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-05 20:07:42 +0200

    Refactor: refactor to use the newly created class

--- course_100_days_of_code/day_18/turtle_art/src/app.py
+++ course_100_days_of_code/day_18/turtle_art/src/app.py
@@ -1,6 +1,10 @@
 """
-This script will create a simple Turtle Art Paint in Python for the 100 Days of Code challenge from the Udemy course.
+Draw a copy of the Hirst painting by using the Turtle package in Python for the 100 Days of Code challenge from the Udemy course.
 
+Requirements:
+    - The number of rows and columns must be 10x10. That means, 10 dots per row, per 10 rows
+    - Each dot must be 20px in size
+    - Each dot must be spaced apart by 50px
 """
 
 import os
@@ -11,34 +15,44 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # --- IMPORT PACKAGES --- #
 from turtle import Turtle, Screen
-from pkgs.move import Shape, random_walk
+from pkgs.hirst_paint import HirstPaint
 
-# instanciate the turtle
-turtle = Turtle(visible=False)
+# --- CONSTANTS --- #
+# change the size of the turtle
+DOT_SIZE = 20
+SPACING = 50
+X_AXIS_POSITION = -240.00
+y_axis_position = -200.00
 
-# define the characteristics of the turtle
-# turtle.shape("circle")
-# turtle.color(randint(0, 255))
-
-# --- SHAPES --- #
-# instantiate the shape object
-shape = Shape()
-
-# Draw all the shapes based on the number of sides input
-# for i in range(3, 11):
-#     shape.geometric_shape(turtle=turtle, number_of_sides=i)
-
-# --- Define a random walk for the turtle --- #
-# random_walk(turtle=turtle, number_of_steps=200)
-shape.spirograph(turtle=turtle, radius=50, angle_head=10)
-
-# --- LINES --- #
-# shape.dashed_line(turtle=turtle)
-
-# --- Screen and Exit --- #
 # Define the screen where the turtle will draw
 # instantiate the screen where the turtle will play
 screen = Screen()
+
+# instantiate a turtle object
+turtle = Turtle(visible=False)
+
+# change the shape of the turtle
+turtle.shape("circle")
+# slowest speed to see what is happenign
+turtle.speed("fastest")
+
+# send the turtle to the bottom right of the canvas
+turtle.penup()
+turtle.goto(X_AXIS_POSITION, y_axis_position)
+turtle.pendown()
+turtle.showturtle()
+
+paint = HirstPaint(
+    turtle=turtle,
+    DOT_SIZE=DOT_SIZE,
+    SPACING=SPACING,
+    X_AXIS_POSITION=X_AXIS_POSITION,
+    y_axis_position=y_axis_position,
+    number_of_loops=10,
+)
+paint.hirst_paint()
+
+# --- Screen and Exit --- #
 screen.colormode(255)
 # once the user clicks on the window, the window will close automatically
 screen.exitonclick()
