2824cb2b58d15293fd290c69c01b148a15096b0c
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-07 22:26:42 +0200

    Add: call method to check the finish line

--- course_100_days_of_code/day_19/etch_a_sketch_app/src/turtle_racing.py
+++ course_100_days_of_code/day_19/etch_a_sketch_app/src/turtle_racing.py
@@ -15,24 +15,31 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from turtle import Turtle, Screen
 from pkgs.racing import TurtleMovement
 
+# --- CONSTANTS --- #
+SCREEN_SIZE_WIDTH = 500
+SCREEN_SIZE_HEIGH = 400
+
 # --- MAIN --- #
 screen = Screen()
 # setup screen size
-screen.setup(width=500, height=400)
+screen.setup(width=SCREEN_SIZE_WIDTH, height=SCREEN_SIZE_HEIGH)
+
+# define a set of characteristics: colors and positions of the turtles
+colors = ["blue", "green", "red", "purple", "yellow"]
+turtle_positions = [-100, -50, 0, 50, 100]
+turtle_list = []
+is_race_on = False
 
 # prompt the user to be able to guess which turtle will be the winner
 guessing_winner = screen.textinput(
     title="Guess the turtle winner",
     prompt="Guess the winning turtle by typing the color:",
 )
-
-
-colors = ["blue", "green", "red", "purple", "yellow"]
-turtle_positions = [-100, -50, 0, 50, 100]
+is_race_on = True
 
 for turtle_index in range(5):
     # create the turtle instances
-    new_turtle = turtle = Turtle(shape="turtle")
+    new_turtle = Turtle(shape="turtle")
     # form the colors list, grab one new color per each turtle
     new_turtle.color(colors[turtle_index])
 
@@ -40,7 +47,26 @@ for turtle_index in range(5):
     turtle_movement = TurtleMovement(
         turtle=new_turtle, x_position=-200, y_position=turtle_positions[turtle_index]
     )
+    # grab each turtle object and insert it into a list to be used to move each turtle afterwards
+    turtle_list.append(turtle_movement)
+    # send the turtles to a specific starting position
     turtle_movement.starting_position()
+
+while is_race_on:
+    for each_turtle in turtle_list:
+        # print("Turtle is: ", each_turtle.turtle.color()[0])
+        each_turtle.turtle_racing()
+        if each_turtle.wall_checker() is False:
+            is_race_on = False
+            if each_turtle.turtle.color()[0] == guessing_winner:
+                print(f"Congratulation, your {guessing_winner} turtle won the race!")
+            else:
+                print(
+                    "Unfortunately, your tutle was not the fastest one. Try again next time!"
+                )
+                print(
+                    f"The winning turtle was the {each_turtle.turtle.color()[0]} one."
+                )
 
 
 screen.exitonclick()
