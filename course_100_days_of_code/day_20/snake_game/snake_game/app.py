ad494342c00a43989268210ab778d08a460dfbc9
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-08 11:40:14 +0200

    Initial Commit

---
+++ course_100_days_of_code/day_20/snake_game/snake_game/app.py
@@ -0,0 +1,15 @@
+"""
+Build the famous Snake game by using the Turtle package in Python for the 100 Days of Code challenge from the Udemy course.
+
+Requirements:
+
+"""
+
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# --- IMPORT PACKAGES --- #
+from turtle import Turtle, Screen
