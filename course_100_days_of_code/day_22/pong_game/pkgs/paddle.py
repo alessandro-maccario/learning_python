536fcb4da8477a77a08ddd6a80986f7bdf62bdb4
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-19 17:10:25 +0200

    Refactor: refactor the Paddle class to accept a starting_position parameter to decide where to insert the paddle once a new instance is created

--- course_100_days_of_code/day_22/pong_game/pkgs/paddle.py
+++ course_100_days_of_code/day_22/pong_game/pkgs/paddle.py
@@ -9,34 +9,36 @@ UP = 90
 DOWN = 270
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-STARTING_POSITIONS = (350, 0)
 
 
 # --- CREATE PADDLE CLASS --- #
 class Paddle:
-    def __init__(self) -> None:
+    def __init__(self, starting_position: str) -> None:
+        """Initialization method for the Paddle class.
+
+        Parameters
+        ----------
+        starting_position : str
+            Define where the puddle should be placed, either left or right on the screen.
+        """
         # instantiate the turtle object by creating the starting body of the paddle with 3 squares
         self.paddle_pieces = []
-        self.starting_position = 0
+        self.starting_position = starting_position
         self.create_paddle()  # initialize the create_paddle method
         self.head = self.paddle_pieces[0]  # grab just the head of the paddle
+
         print(self.head.heading())
 
     def create_paddle(self):
-        # for position in STARTING_POSITIONS:
-        self.add_segment(STARTING_POSITIONS)
-
-    def add_segment(self, position):
         """Create the turtle and position it"""
         turtle = Turtle(shape="square")
         turtle.color("White")
         turtle.fillcolor("white")
         turtle.up()
-        turtle.goto(STARTING_POSITIONS)
+        turtle.goto(self.starting_position)
         turtle.shapesize(stretch_len=5, stretch_wid=1)
         turtle.setheading(UP)
         self.paddle_pieces.append(turtle)
-        self.starting_position -= 20
 
     def move_up(self):
         """Move the turtle UP using the key strokes"""
