e9fd6723d22c98bcf623a57fdbf944c907413e38
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-22 19:03:02 +0200

    Add: add collision methods

--- course_100_days_of_code/day_22/pong_game/pkgs/ball.py
+++ course_100_days_of_code/day_22/pong_game/pkgs/ball.py
@@ -19,5 +19,24 @@ class Ball(Turtle):
         self.up()
         self.goto((0, 0))
 
+        # Movement step size for x and y directions
+        self.x_move = 0.3  # larger values will move the ball faster and viceversa
+        self.y_move = 0.3
+
     def ball_movement(self):
-        self.goto(self.xcor() + 1, self.ycor() + 1)
+        """Keep the ball moving based on a certain amount"""
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def wall_bouncing(self):
+        """If the ball hits the wall, then change the y_move direction"""
+        self.y_move *= -1
+
+    def paddle_bouncing(self):
+        """Revert the x_move direction"""
+        self.x_move *= -1
+
+    def restart(self):
+        # send the ball to the homebase
+        self.home()
+        # start again the ball movement in reverse
+        self.paddle_bouncing()
