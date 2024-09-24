e1f38d8665eb063023849c2b50e9e3736fcd06ef
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-24 21:25:59 +0200

    Add: add top explanation about the app.py

---
+++ course_100_days_of_code/day_23/turtle_crossing_game/turtle_crossing_game/app.py
@@ -0,0 +1,19 @@
+"""
+Build a Turtle Crossing game by using the Turtle package in Python for the 100 Days of Code challenge from the Udemy course.
+
+Reference
+    -
+
+Problem Breakdown:
+    1.
+    2.
+
+Requirements:
+    -
+"""
+
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
