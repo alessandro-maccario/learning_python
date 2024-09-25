272f56837b1c41f6cf1aa8482c50c31c099a1958
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-09-25 07:28:09 +0200

    Add: add basic functionalities about the screen and the turtle animations

--- course_100_days_of_code/day_23/turtle_crossing_game/turtle_crossing_game/app.py
+++ course_100_days_of_code/day_23/turtle_crossing_game/turtle_crossing_game/app.py
@@ -9,7 +9,8 @@ Problem Breakdown:
     2.
 
 Requirements:
-    -
+    - Create a Turtle screen that is 600px by 600px.
+    - You need to turn off tracer(0) and use update() to refresh the screen every 0.1s.
 """
 
 import os
@@ -17,3 +18,27 @@ import sys
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from turtle import Turtle, Screen
+
+# --- CONSTANTS --- #
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+# --- SCREEN --- #
+# Instantiate the screen and the screen size
+screen = Screen()
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)  # turn off animation so the paddle directly goes into position
+
+
+# after turning off the animation, we manually have to turn constantly the animation on
+game_is_on = True
+while game_is_on:
+    screen.update()  # after turning off the animation, you need to manually turn on the update
+
+
+# let the screen on until the user clicks on it
+screen.exitonclick()
