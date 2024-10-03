5182a95464fdb21393cb4f885df7741a52f9ed5f
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-03 17:50:12 +0200

    Add: add background image and set the screen size

--- course_100_days_of_code/day_25/us_states_map_quiz/us_states_map_quiz/main.py
+++ course_100_days_of_code/day_25/us_states_map_quiz/us_states_map_quiz/main.py
@@ -11,3 +11,29 @@ Requirements:
     - The Turtle package works only with the .gif image format.
 
 """
+
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# --- IMPORT PACKAGES --- #
+from turtle import Turtle, Screen
+
+
+# --- CONSTANTS --- #
+SCREEN_WIDTH = 725
+SCREEN_HEIGHT = 491
+
+
+# Instantiate the screen and the screen size
+screen = Screen()
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.title("US Map Quiz")
+# now set the background to our space image
+screen.bgpic("day_25/us_states_map_quiz/attachments/blank_states_img.gif")
+
+
+# let the screen on until the user clicks on it
+screen.exitonclick()
