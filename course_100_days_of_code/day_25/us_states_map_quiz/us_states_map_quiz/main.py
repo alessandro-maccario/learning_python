c645160548c6b8016eed7071863fbc8fe0af9bfa
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-03 17:45:37 +0200

    Initial commit

---
+++ course_100_days_of_code/day_25/us_states_map_quiz/us_states_map_quiz/main.py
@@ -0,0 +1,13 @@
+"""
+Script to create the US States Map Quiz by using Python for the 100 Days of Code challenge from the Udemy course.
+
+Reference
+    - https://www.sporcle.com/games/g/states
+
+Problem Breakdown:
+    1.
+
+Requirements:
+    - The Turtle package works only with the .gif image format.
+
+"""
