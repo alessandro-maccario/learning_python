6fce72f8b69861991bbf5c1a0608db002fca7f88
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-07 20:59:24 +0200

    Initial commit

---
+++ course_100_days_of_code/day_27/mile2km_converter/mile2km_converter/main.py
@@ -0,0 +1,12 @@
+"""
+Script to create a simple Miles to KM converter by using Python for the 100 Days of Code challenge from the Udemy course.
+
+Problem Breakdown:
+    1.
+    2.
+
+
+Requirements:
+    -
+
+"""
