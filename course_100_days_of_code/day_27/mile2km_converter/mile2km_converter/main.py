86868bafda867777cfce047e607d14f9868f1841
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-09 07:34:39 +0200

    Add: add the minimum element to create a tkinter window

--- course_100_days_of_code/day_27/mile2km_converter/mile2km_converter/main.py
+++ course_100_days_of_code/day_27/mile2km_converter/mile2km_converter/main.py
@@ -10,3 +10,26 @@ Requirements:
     -
 
 """
+
+# --- IMPORT PACKAGES --- #
+import tkinter
+
+# instantiate the tkinter window
+window = tkinter.Tk()
+window.title("Mile2KM Converter")
+# define a minimum size: if there are more elements or the user resizes the window, it is allowed
+window.minsize(width=500, height=300)
+
+
+"""
+    Components:
+        - first instantiate the component
+        - define how to visualize it to show it
+"""
+# label component
+lab = tkinter.Label(text="Label 1", font=("Arial", 15, "bold"))
+lab.pack(side="left")  # pack the label on the screen and automatically center it
+
+
+# keep the window on the screen and wait for user's input
+window.mainloop()
