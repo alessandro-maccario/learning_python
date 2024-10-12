80aa642ed5982da567e2e05f69b658f7c4756a6a
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-12 20:20:59 +0200

    Refactor: refactor code by using methods and fix layout

--- course_100_days_of_code/day_29/password_manager/pkgs/UI.py
+++ course_100_days_of_code/day_29/password_manager/pkgs/UI.py
@@ -5,36 +5,7 @@ import tkinter as tk
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from pkgs.constants import PINK, RED, GREEN, YELLOW, FONT_NAME
-
-
-class PasswordManagerUIOLD:
-    def __init__(self) -> None:
-        self.window = tk.Tk()
-        self.window.title("Password Manager")
-        self.window.geometry("600x400")
-        self.window.config(padx=120, pady=10, bg="grey")
-
-        # instantiate a Canvas where the image will be laid out
-        # highlightthickness=0 -> needed to remove a white border that was still there
-        self.canvas = tk.Canvas(width=200, height=200, bg="white", highlightthickness=0)
-        self.locker_pic = tk.PhotoImage(
-            file="day_29/password_manager/attachments/logo.png"
-        )
-        self.canvas.create_image(100, 100, image=self.locker_pic)
-        self.canvas.grid(row=0, column=1)
-
-        # timer label component
-        self.website_label = tk.Label(
-            text="Website", font=(FONT_NAME, 11, "bold"), bg="white"
-        )
-        self.website_label.grid(row=1, column=0)
-
-        # Input component
-        self.website_input_component = tk.Entry(width=35)
-        self.website_input_component.grid(row=1, column=1, columnspan=3)
-
-        self.window.mainloop()
+from pkgs.constants import PINK, RED, GREEN, YELLOW, WHITE, FONT_NAME
 
 
 class PasswordManagerUI:
@@ -43,20 +14,20 @@ class PasswordManagerUI:
         self.setup_canvas()
         self.setup_labels()
         self.setup_input_components()
+        self.setup_buttons()
         self.window.mainloop()
 
     def setup_window(self):
         """Initializes the main window settings."""
         self.window = tk.Tk()
         self.window.title("Password Manager")
-        self.window.geometry("600x400")
-        self.window.config(padx=120, pady=10, bg="grey")
+        self.window.config(padx=50, pady=50, bg=WHITE)
 
     def setup_canvas(self):
         """Initializes the canvas and adds the image."""
         # instantiate a Canvas where the image will be laid out
         # highlightthickness=0 -> needed to remove a white border that was still there
-        self.canvas = tk.Canvas(width=200, height=200, bg="white", highlightthickness=0)
+        self.canvas = tk.Canvas(width=200, height=200, bg=WHITE, highlightthickness=0)
         self.locker_pic = tk.PhotoImage(
             file="day_29/password_manager/attachments/logo.png"
         )
@@ -65,14 +36,36 @@ class PasswordManagerUI:
 
     def setup_labels(self):
         """Creates and places labels."""
-        # timer label component
-        self.website_label = tk.Label(
-            text="Website", font=(FONT_NAME, 11, "bold"), bg="white"
-        )
-        self.website_label.grid(row=1, column=0)
+        # website label
+        self.website_label = tk.Label(text="Website", bg=WHITE)
+        self.website_label.grid(row=1, column=0, sticky="w")
+
+        # email/username label
+        self.email_username = tk.Label(text="Email/Username", bg=WHITE)
+        self.email_username.grid(row=2, column=0, sticky="w")
+
+        # password label
+        self.password = tk.Label(text="Password", bg=WHITE)
+        self.password.grid(row=3, column=0, sticky="w")
 
     def setup_input_components(self):
         """Creates and places input components."""
-        # Input component
-        self.website_input_component = tk.Entry(width=35)
-        self.website_input_component.grid(row=1, column=1, columnspan=3)
+        # Input component for website
+        self.website_input_component = tk.Entry(width=51)
+        self.website_input_component.grid(row=1, column=1, columnspan=2)
+
+        # Input component for email/username
+        self.email_username_input_component = tk.Entry(width=51)
+        self.email_username_input_component.grid(row=2, column=1, columnspan=2)
+
+        # Input component for password
+        self.password_input_component = tk.Entry(width=32)
+        self.password_input_component.grid(row=3, column=1)
+
+    def setup_buttons(self):
+        """Create the Generate password button and the Add button"""
+        self.generate_passwd_button = tk.Button(text="Generate Password")
+        self.generate_passwd_button.grid(row=3, column=2)
+
+        self.add_button = tk.Button(text="Add", width=43)
+        self.add_button.grid(row=4, column=1, columnspan=2)
