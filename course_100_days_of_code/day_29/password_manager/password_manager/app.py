0d2bd9117004f92077a5ce63ac83c9d058e6ea55
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-12 20:21:30 +0200

    Add: start reloader to always automatically reload the window and define main function

--- course_100_days_of_code/day_29/password_manager/password_manager/app.py
+++ course_100_days_of_code/day_29/password_manager/password_manager/app.py
@@ -18,7 +18,31 @@ Requirements:
 # --- IMPORT PACKAGES --- #
 import os
 import sys
+import hupper  # for interactive update of the tkinter window after every changes to the code
 import tkinter as tk
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pkgs.UI import PasswordManagerUI
+
+
+def start_reloader():
+    """Reload the app at every changes in the code"""
+    hupper.start_reloader("app.main")
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+def main():
+    # instantiate the class PasswordManagerApp
+    ui = PasswordManagerUI()
+
+
+if __name__ == "__main__":
+    start_reloader()
