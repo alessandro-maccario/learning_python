228c6f954bde4cd8dafa6e3d4b55da7a26dac601
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-12 08:14:57 +0200

    Initial commit password manager

---
+++ course_100_days_of_code/day_29/password_manager/password_manager/app.py
@@ -0,0 +1,24 @@
+"""
+Script to create a Pomodoro Technique App by using Python for the 100 Days of Code challenge from the Udemy course.
+
+References
+    1. https://docs.python.org/3/library/tk.html
+    2. https://www.tcl.tk/man/tcl8.5/TkCmd/event.htm
+
+Problem Breakdown:
+    1.
+    2.
+
+
+Requirements:
+    -
+
+"""
+
+# --- IMPORT PACKAGES --- #
+import os
+import sys
+import tkinter as tk
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
