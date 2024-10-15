ae4ce42ee2e2969306843c8090d3b6864eb61778
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-15 19:18:08 +0200

    Add: password generation and filling into the password entry component

--- course_100_days_of_code/day_29/password_manager/pkgs/generate_password.py
+++ course_100_days_of_code/day_29/password_manager/pkgs/generate_password.py
@@ -2,16 +2,16 @@
 import tkinter as tk
 import secrets  # https://docs.python.org/3/library/secrets.html
 import string  # https://docs.python.org/3/library/string.html#string.ascii_lowercase
-import clipboard
-from pkgs.constants import SPECIAL_CHARS
+# import clipboard
 
 
 class GeneratePassword:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, password_input_component: str) -> None:
+        self.password_input_component = password_input_component
+        self.generate_password()
 
     # --- Password generator function --- #
-    def generate_password() -> None:
+    def generate_password(self) -> None:
         """
         It uses the string module to get the letters and digits thath make up
         the alphabet used to generate the random characters. These
@@ -28,10 +28,23 @@ class GeneratePassword:
         """
         letters = string.ascii_letters
         digits = string.digits
-        alphabet = letters + digits
         special_chars = string.punctuation
+        alphabet_special_chars = letters + digits + special_chars
         pwd_length = 14  # you can change it with your own decision
         pwd = ""  # empty string to start with
 
-        for i in range(pwd_length):
-            pwd += "".join(secrets.choice(alphabet))
+        for _ in range(pwd_length):
+            pwd += "".join(secrets.choice(alphabet_special_chars))
+
+        return pwd
+
+    def fill_password_entry(self):
+        """Generate a new password and"""
+        # Generate a new password
+        button_generated_password = self.generate_password()
+
+        # Set the new password in the password input component
+        self.password_input_component.delete(0, tk.END)  # Clear any previous text
+        self.password_input_component.insert(
+            0, button_generated_password
+        )  # Insert the new password
