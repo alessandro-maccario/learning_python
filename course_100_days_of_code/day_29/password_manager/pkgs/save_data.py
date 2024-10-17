a6fb5829458245427afcbe009aa55a79d038db8f
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-17 19:39:36 +0200

    Add: add method save2json to save the data to a JSON file

--- course_100_days_of_code/day_29/password_manager/pkgs/save_data.py
+++ course_100_days_of_code/day_29/password_manager/pkgs/save_data.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
+import json
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pkgs.constants import SAVE_PATH
 
 
 class Add2CSV:
@@ -54,6 +62,47 @@ class Add2CSV:
                     "day_29/password_manager/data/passwords.csv", "a"
                 ) as pass_csv:
                     pass_csv.write(f"\n{website}, {email_username}, {password}")
+
+                self.clear_entries()
+                messagebox.showinfo("Saved", "Saved successfully!")
+        else:
+            pass
+
+    def save2json(self):
+        """Save the input to the json"""
+
+        # if something has been inserted in website, email/username and password AND the confirmation is true
+        if self.check_input_validity():
+            # ask the user if they are sure that they want to save this data
+            ask_user_confirmation = messagebox.askyesno(
+                title="Saving confirmation", message="Do you want to save the data?"
+            )
+            if ask_user_confirmation:
+                website, email_username, password = self.check_input_validity()
+
+                # TODO: recheck the code to be sure to understand it
+                # Check if the file exists and is not empty: ensures that json.load() is only called if the file has content.
+                """Appending the JSON: Instead of appending directly using mode="a", you should open the file in write mode (mode="w") and overwrite it with the updated JSON. JSON needs to be written as a whole object, not appended line-by-line like in plain text files."""
+                if not os.path.isfile(SAVE_PATH) or os.stat(SAVE_PATH).st_size == 0:
+                    feeds = []
+
+                else:
+                    with open(SAVE_PATH, "r") as feedsjson:
+                        feeds = json.load(feedsjson)  # Load existing data
+
+                # Data to be written
+                dictionary = {
+                    "Website": website,
+                    "Email/Username": email_username,
+                    "Password": password,
+                }
+
+                # Append new entry to the feeds list
+                feeds.append(dictionary)
+
+                # Write the updated list back to the file
+                with open(SAVE_PATH, mode="w") as f:
+                    json.dump(feeds, f, indent=4)  # dump, not dumps, to write to a file
 
                 self.clear_entries()
                 messagebox.showinfo("Saved", "Saved successfully!")
