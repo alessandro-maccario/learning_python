f427b539e51ff714b0f56b1e479f03a9842c9e82
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-18 20:09:51 +0200

    Add: add find data class

---
+++ course_100_days_of_code/day_29/password_manager/pkgs/find_data.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+import json
+import os
+import sys
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pkgs.constants import SAVE_PATH
+
+
+class FindData:
+    pass
