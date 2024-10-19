189f6e894e4f3a05a1decab7effa72a2be5cf990
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-19 23:09:46 +0200

    Refactor: refactor messagebox info

--- course_100_days_of_code/day_29/password_manager/pkgs/find_data.py
+++ course_100_days_of_code/day_29/password_manager/pkgs/find_data.py
@@ -21,20 +21,20 @@ class FindData:
             with open(SAVE_PATH, "r") as feedsjson:
                 # reading existing data
                 feeds = json.load(feedsjson)
-
-                if website in feeds:
-                    messagebox.showinfo(
-                        f"{website}",
-                        f"Email/Username:\n{feeds[website]["Email/Username"]} \n\nPassword:\n{feeds[website]["Password"]}",
-                    )
-                else:
-                    messagebox.showinfo(
-                        "Error",
-                        "No details for the website found",
-                    )
         except (ValueError, FileNotFoundError):
             # add a popup that says "Entry not found!"
             messagebox.showinfo(
                 "Error",
                 "No Data file found",
             )
+        else:  # it will run if the try succeded
+            if website in feeds:
+                messagebox.showinfo(
+                    f"{website}",
+                    f"Email/Username:\n{feeds[website]["Email/Username"]} \n\nPassword:\n{feeds[website]["Password"]}",
+                )
+            else:
+                messagebox.showinfo(
+                    "Error",
+                    f"No details for the {website} found",
+                )
