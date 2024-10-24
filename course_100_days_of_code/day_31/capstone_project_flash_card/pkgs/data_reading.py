e3279f76f07b1a3dce0ced335867f0e40c01a210
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-24 17:49:58 +0200

    Add: add random selection of words

--- course_100_days_of_code/day_31/capstone_project_flash_card/pkgs/data_reading.py
+++ course_100_days_of_code/day_31/capstone_project_flash_card/pkgs/data_reading.py
@@ -1,12 +1,22 @@
 import pandas as pd
+from random import choice
+
+from pkgs.constants import DE_EN_DATA
 
 
 class GermanEnglishTranslation:
     def __init__(self) -> None:
         pass
 
-    def data_reader(self):
-        df = pd.read_csv(
-            "day_31/capstone_project_flash_card/data/de_100_words.txt", sep=","
-        )
-        return df
+    def data_reader(self) -> dict:
+        df = pd.read_csv(DE_EN_DATA, sep=",")
+        df_to_dict = df.to_dict(orient="records")
+        return df_to_dict
+
+    def random_translation_selection(self) -> dict:
+        """Pick a random dictionry from the data_reader"""
+        return choice(self.data_reader())
+
+    def dict_2_list(self) -> list:
+        """Take the random_translation_selection and make a list out of it"""
+        return self.random_translation_selection().values()
