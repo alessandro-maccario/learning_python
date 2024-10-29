d943f94d3c6ce3f3f77cd387c8eaf9c0942cada2
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-29 20:29:24 +0100

    Add: add scopes constant

--- course_100_days_of_code/day_32/email_downloader_and_parser/pkgs/constants.py
+++ course_100_days_of_code/day_32/email_downloader_and_parser/pkgs/constants.py
@@ -1,1 +1,5 @@
 USER_ID = "me"
+TOKEN_JSON = "day_32/email_downloader_and_parser/tokens/token.json"
+CLIENTID_JSON = "day_32/email_downloader_and_parser/tokens/clientID.json"
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
