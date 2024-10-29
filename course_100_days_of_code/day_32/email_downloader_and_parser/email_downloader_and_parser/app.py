7a2902f02a551f433207466f1ac0a6dee4f714ac
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-29 20:29:49 +0100

    Refactor: refactor code for modularization

--- course_100_days_of_code/day_32/email_downloader_and_parser/email_downloader_and_parser/app.py
+++ course_100_days_of_code/day_32/email_downloader_and_parser/email_downloader_and_parser/app.py
@@ -4,54 +4,41 @@ import sys
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from pkgs.constants import USER_ID
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+from pkgs.constants import USER_ID, TOKEN_JSON, CLIENTID_JSON, SCOPES
+from pkgs.email_fetching import EmailFetching
 
 
 def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+    """Shows basic usage of the Gmail API."""
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # TODO: the path should be moved to constants
+    if os.path.exists(TOKEN_JSON):
+        creds = Credentials.from_authorized_user_file(TOKEN_JSON, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "day_32/email_downloader_and_parser/tokens/clientID.json", SCOPES
-            )
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENTID_JSON, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("day_32/email_downloader_and_parser/tokens/token.json", "w") as token:
+        with open(TOKEN_JSON, "w") as token:
             token.write(creds.to_json())
 
     try:
         # Call the Gmail API
-        service = build("gmail", "v1", credentials=creds)
-        print(service)
-        # results = service.users().labels().list(userId=USER_ID).execute()
-        # labels = results.get("labels", [])
-
-        # if not labels:
-        #     print("No labels found.")
-        #     return
-        # print("Labels:")
-        # for label in labels:
-        #     print(label["name"])
+        gmail_fetching = EmailFetching()
+        gmail_fetching.fetch_emails_with_subject(
+            creds, user_id=USER_ID, subject="LinkedIn", num_results=10
+        )
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
