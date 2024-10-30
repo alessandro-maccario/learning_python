0ccee67cd0b16c69156628a7c51665dabe5126f9
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-10-30 20:59:14 +0100

    Add: add filepaths where to save csv and json

--- course_100_days_of_code/day_32/email_downloader_and_parser/pkgs/constants.py
+++ course_100_days_of_code/day_32/email_downloader_and_parser/pkgs/constants.py
@@ -3,3 +3,5 @@ TOKEN_JSON = "day_32/email_downloader_and_parser/tokens/token.json"
 CLIENTID_JSON = "day_32/email_downloader_and_parser/tokens/clientID.json"
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+PATH_SAVE_DATA_CSV = "day_32/email_downloader_and_parser/data/test_gmail.csv"
+PATH_SAVE_DATA_JSON = "day_32/email_downloader_and_parser/data/test_gmail.json"
