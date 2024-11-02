b36bf64385b07c244fe0f62c897bd1f9b16ad049
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-02 16:56:12 +0100

    Refactor: refactor the csv name to save the file with the correct past_days_forecase and timedelta forecast

--- course_100_days_of_code/day_33/weather_forecast/weather_forecast/app.py
+++ course_100_days_of_code/day_33/weather_forecast/weather_forecast/app.py
@@ -6,13 +6,17 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pkgs.data_fetching import DataFetching
 from pkgs.plots import PlotlyPlot
+from pkgs.constants import TIMEDELTA_DATE_FORECAST, PAST_DAYS_FORECAST
 
 
 def main():
     data_fetching = DataFetching()
     df = data_fetching.open_weather_request_forecast()
-    print(df)
-    df.to_csv("day_33/weather_forecast/data/out.csv", index=False)
+    # print(df)
+    df.to_csv(
+        f"day_33/weather_forecast/data/forecast_past_{PAST_DAYS_FORECAST}_future_{TIMEDELTA_DATE_FORECAST}.csv",
+        index=False,
+    )
 
     # # plot the forecast up to n days
     # plotly_plot = PlotlyPlot()
