7ed286146485f99abe1155d1fd7d575a7be9e1ae
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-03 21:11:30 +0100

    Add: add wmo_codes to convert the JSON codes to dataframe

---
+++ course_100_days_of_code/day_33/weather_forecast/pkgs/wmo_codes.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import json
+import pandas as pd
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pkgs.constants import WMO_CODES
+
+
+class WMOProcessing:
+    def __init__(self) -> None:
+        pass
+
+    def wmo_json_2_pandas(self) -> pd.DataFrame:
+        # read the wmo_codes.json data in
+        data = pd.read_json(WMO_CODES)
+        # hold all the "day" values inside the list_wmo_code
+        list_wmo_code = []
+        for key_wmo_code, value in data.items():
+            # add the wmo code to the "day" dictionary to easy the conversion to a DataFrame
+            value["day"].update({"wmo_code": key_wmo_code})
+            list_wmo_code.append(value["day"])
+
+        # convert dictionary to pd.DataFrame to be used in conjunction with the weather data and add the wmo_code
+        df = pd.DataFrame.from_records(list_wmo_code)
+
+        return df
+
+
+# data = pd.read_json("data/wmo_codes.json")
+
+# # hold all the "day" values inside the list_wmo_code
+# list_wmo_code = []
+# for key_wmo_code, value in data.items():
+#     # add the wmo code to the "day" dictionary to easy the conversion to a DataFrame
+#     value["day"].update({"wmo_code": key_wmo_code})
+#     list_wmo_code.append(value["day"])
+
+# # convert dictionary to pd.DataFrame to be used in conjunction with the weather data and add the wmo_code
+# df = pd.DataFrame.from_records(list_wmo_code)
+# print(df)
