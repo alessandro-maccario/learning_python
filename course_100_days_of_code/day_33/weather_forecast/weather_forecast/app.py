530986868c5236a03add2d190063e26aebf06261
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-03 21:11:52 +0100

    Add: add join for historical, forecast and wmo_code data

--- course_100_days_of_code/day_33/weather_forecast/weather_forecast/app.py
+++ course_100_days_of_code/day_33/weather_forecast/weather_forecast/app.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import pandas as pd
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pkgs.data_fetching import DataFetching
+from pkgs.wmo_codes import WMOProcessing
 from pkgs.plots import PlotlyPlot
 from pkgs.constants import (
     TIMEDELTA_DATE_FORECAST,
@@ -15,15 +17,28 @@ from pkgs.constants import (
 
 def main():
     data_fetching = DataFetching()
+    wmo_codes = WMOProcessing()
+    # grab the forecast df
     df_forecast = data_fetching.open_weather_request_forecast()
+    # grab the historical df
     df_historical = data_fetching.open_weather_request_historical()
+    # grab the wmo_codes df
+    df_wmo_codes = wmo_codes.wmo_json_2_pandas()
+
+    # join the forecast and historical with the wmo_codes
+    df_forecast_wmo = pd.merge(
+        df_forecast, df_wmo_codes, left_on="weather_code", right_on="wmo_code"
+    )
+    df_historical_wmo = pd.merge(
+        df_historical, df_wmo_codes, left_on="weather_code", right_on="wmo_code"
+    )
 
     # save the data to a csv
-    df_forecast.to_csv(
+    df_forecast_wmo.to_csv(
         "data/forecast_data.csv",
         index=False,
     )
-    df_historical.to_csv(
+    df_historical_wmo.to_csv(
         "data/historical_data.csv",
         index=False,
     )
