5eb29f38b22b6302a6954c4b07b3b8248e462846
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-03 12:28:38 +0100

    Add: add checking in case the seconds/minutes are None

--- course_100_days_of_code/day_33/weather_forecast/pkgs/time_conversion.py
+++ course_100_days_of_code/day_33/weather_forecast/pkgs/time_conversion.py
@@ -1,8 +1,12 @@
+import numpy as np
+import pandas as pd
+
+
 class TimeConversion:
     def __init__(self) -> None:
         pass
 
-    def seconds2minutes(self, seconds: float) -> float:
+    def seconds2minutes(self, seconds: np.array) -> np.array:
         """Convert the seconds to minutes
 
         Parameters
@@ -15,7 +19,11 @@ class TimeConversion:
         float
             Return the converted seconds to minutes
         """
-        return seconds / 60
+        if seconds is None:
+            pass
+        else:
+            seconds = seconds / 60
+        return seconds
 
     def minutes2hours(self, minutes: float) -> float:
         """Convert the minutes to hours
@@ -25,4 +33,8 @@ class TimeConversion:
         float
             Return the converted minutes to hours
         """
-        return minutes / 60
+        if minutes is None:
+            pass
+        else:
+            minutes = minutes / 60
+        return minutes
