0336a11ed385537a56df6545f39bfaad7887d6ef
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-04 19:06:51 +0100

    Add: add bar_plot_forecast_historical_emoji plot

--- course_100_days_of_code/day_33/weather_forecast/pkgs/plots.py
+++ course_100_days_of_code/day_33/weather_forecast/pkgs/plots.py
@@ -40,3 +40,53 @@ class PlotlyPlot:
         )
 
         fig.show()
+
+    def bar_plot_forecast_historical_emoji(self, df: pd.DataFrame) -> None:
+        """Create a side-by-side bar plot to show the difference in the average
+        temperature between forecast and historical where the markers on top
+        of each bar are represented by the forecasted and historical emoji related
+        to the weather condition based on the wmo_codes.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Dataframe to be plotted
+        """
+
+        # Create the plot
+        fig = go.Figure()
+
+        # Add historical temperature bars
+        fig.add_trace(
+            go.Bar(
+                x=df["time_forecast"],
+                y=df["temperature_average_historical"],
+                name="Historical Temperature",
+                text=df["emoji_historical"],
+                textposition="outside",
+                marker_color="lightgrey",
+            )
+        )
+        # Add forecast temperature bars
+        fig.add_trace(
+            go.Bar(
+                x=df["time_forecast"],
+                y=df["temperature_average_forecast"],
+                name="Forecast Temperature",
+                text=df["emoji_forecast"],
+                textposition="outside",
+                marker_color="skyblue",
+            )
+        )
+
+        # Customize layout for grouped bars and axis titles
+        fig.update_layout(
+            barmode="group",
+            title="Forecast vs. Historical Temperatures and Weather conditions",
+            xaxis_title="Date",
+            yaxis_title="Temperature (°C)",
+            xaxis=dict(tickformat="%Y-%m-%d"),  # Format dates on x-axis
+        )
+
+        # Show plot
+        fig.show()
