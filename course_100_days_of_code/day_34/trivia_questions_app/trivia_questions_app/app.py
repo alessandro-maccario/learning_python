df3fc15b1d60913544c2e27011bf7613d6924baf
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-06 20:58:06 +0100

    Refactor: remove unnecessary code and instantiate the trivia_quiz_ui class

--- course_100_days_of_code/day_34/trivia_questions_app/trivia_questions_app/app.py
+++ course_100_days_of_code/day_34/trivia_questions_app/trivia_questions_app/app.py
@@ -1,30 +1,53 @@
 import os
 import sys
-import json
-import base64
 import html
-import pandas as pd
 import requests
+
+# import hupper  # for interactive update of the tkinter window after every changes to the code
+import hupper  # for interactive update of the tkinter window after every changes to the code
 
 # dynamically adjust the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from pkgs.constants import BASE_API_URL
+from pkgs.UI import TriviaQuizUI
+from pkgs.constants import BASE_API_URL, QUESTION_TEST
 
-
-# define the parameter for the query
-params = {"amount": 10, "difficulty": "easy"}
+# define the parameter for the query (currently, only True/False)
+params = {"amount": 10, "difficulty": "easy", "type": "boolean"}
 # send a request to the Open Trivia API
 otapi_request = requests.get(BASE_API_URL, params=params)
 otapi_content = otapi_request.json()["results"]
 
-is_game_on = True
-while is_game_on:
-    for each_question in otapi_content:
-        question = html.unescape(each_question["question"])
-        print(question)
-        user_answer = input("Insert your answer:")
+# TODO:
+# 1. create a dictionary where the key is the question and the value is the answer
+# TODO: 2. pick one random question at a time and pass it inside the text to be shown in the canvas
 
-# # Open the JSON file
-# with open("day_34/trivia_questions_app/data/trivia_questions.json", "w") as f:
-#     data = json.dump(otapi_content, f)
+# 1. create a dictionary where the key is the question and the value is the answer
+question_dict = {}
+for each_question in otapi_content:
+    question = html.unescape(each_question["question"])
+    correct_answer = html.unescape(each_question["correct_answer"])
+
+    if question not in question_dict:
+        question_dict[question] = correct_answer
+    question_dict.update({question: correct_answer})
+
+
+def start_reloader():
+    """Reload the app at every changes in the code"""
+    hupper.start_reloader("app.main")
+
+
+def main():
+    # Instantiate the TriviaQuizUI
+    trivia_quiz_ui = TriviaQuizUI(question_dict=question_dict)
+
+    is_game_on = True
+    while is_game_on:
+        # stop the game when reached the end of the list of question
+        is_game_on = False
+
+
+if __name__ == "__main__":
+    # main()
+    start_reloader()
