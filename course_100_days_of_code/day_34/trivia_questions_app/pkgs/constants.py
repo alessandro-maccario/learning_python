454779056e33a4955c44ed50c7d273630f8d1ea6
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-06 20:57:15 +0100

    Add: add the CARD_FRONT path

--- course_100_days_of_code/day_34/trivia_questions_app/pkgs/constants.py
+++ course_100_days_of_code/day_34/trivia_questions_app/pkgs/constants.py
@@ -1,1 +1,16 @@
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WHITE = "#FFFFFF"
+BACKGROUND = "#b1ddc6"
+CANVAS_WIDTH = 350
+CANVAS_HEIGHT = 450
+TRUE_BUTTON_IMAGE = "day_34/trivia_questions_app/attachments/true.png"
+FALSE_BUTTON_IMAGE = "day_34/trivia_questions_app/attachments/false.png"
+X_WORD_PLACE = 150
+Y_WORD_PLACE = 200
 BASE_API_URL = "https://opentdb.com/api.php"
+CARD_FRONT = "day_34/trivia_questions_app/attachments/card_front.png"
