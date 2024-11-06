896ac5bfd96443899b08b3d74ed992b3b4b82581
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-06 20:57:42 +0100

    Add: add the pick_random_question, validate_answer methods

---
+++ course_100_days_of_code/day_34/trivia_questions_app/pkgs/UI.py
@@ -0,0 +1,171 @@
+import os
+import sys
+import tkinter as tk
+from PIL import Image, ImageTk
+import customtkinter as ctk
+from random import choice
+
+# dynamically adjust the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pkgs.constants import (
+    BACKGROUND,
+    CANVAS_HEIGHT,
+    CANVAS_WIDTH,
+    TRUE_BUTTON_IMAGE,
+    FALSE_BUTTON_IMAGE,
+    CARD_FRONT,
+    X_WORD_PLACE,
+    Y_WORD_PLACE,
+    QUESTION_TEST,
+)
+
+
+class TriviaQuizUI:
+    def __init__(self, question_dict: dict) -> None:
+        # Initialize with a list of questions
+        self.questions = question_dict
+        self.current_answer = None  # Store the answer for the current question
+        self.score_point = 0
+        self.setup_window()
+        self.setup_canvas()
+        self.setup_labels()
+        self.setup_buttons()
+        self.window.mainloop()
+
+    def setup_window(self):
+        """Initializes the main window settings."""
+        self.window = tk.Tk()
+        self.window.title("Trivia Quiz Game")
+        self.window.config(padx=10, pady=0)
+        self.window.configure(background=BACKGROUND)
+
+        # position the Tkinter window at the center of the screen
+        w = self.window.winfo_reqwidth()
+        h = self.window.winfo_reqheight()
+        ws = self.window.winfo_screenwidth()
+        hs = self.window.winfo_screenheight()
+        x = (ws / 2.5) - (w / 2.5)
+        y = (hs / 6) - (h / 6)
+        self.window.geometry("+%d+%d" % (x, y))
+        self.flashcard_picture_front = tk.PhotoImage(file=CARD_FRONT)
+
+    def setup_canvas(self):
+        """Initializes the canvas and adds the image."""
+        # instantiate a Canvas where the image will be laid out
+        self.canvas = ctk.CTkCanvas(
+            width=CANVAS_WIDTH,
+            height=CANVAS_HEIGHT,
+            highlightthickness=0,
+            bg=BACKGROUND,
+        )
+        self.flashcard_picture_front = tk.PhotoImage(file=CARD_FRONT)
+        self.canvas.create_image(
+            CANVAS_WIDTH / 2,
+            CANVAS_HEIGHT / 2,
+            image=self.flashcard_picture_front,
+            tags=(
+                "card_front"
+            ),  # give a tag to be referenced when flipping to the back card
+        )
+        self.canvas.grid(row=0, column=0, columnspan=2)
+
+    def setup_labels(self):
+        """Creates and place the translated text on the canvas."""
+
+        # delete previous displayed word/title before generating the new random one
+        self.canvas.delete("tag_word")
+
+        # Fetch a new question each time
+        new_question, answer = self.pick_random_question(self.questions)
+        self.current_answer = answer  # Save the current answer for validation
+
+        # once selected the question is selected, remove it from the dictionary
+        removed_value = self.questions.pop(new_question)
+
+        # do not need to use grid, because you are already placing the text by using x and y
+        self.canvas.create_text(
+            X_WORD_PLACE,
+            Y_WORD_PLACE,
+            text=new_question,  # add the new question as test into the canvas
+            font=("Arial", 15),
+            tag="tag_word",  # assign a tag to be used for deletion
+            width=300,
+        )
+
+    def setup_buttons(self):
+        """Create the Accept and Refuse buttons"""
+
+        # TRUE button
+        true_button_image = ctk.CTkImage(
+            light_image=Image.open(TRUE_BUTTON_IMAGE),
+            size=(80, 80),
+        )
+
+        self.accept_button = ctk.CTkButton(
+            master=self.window,
+            image=true_button_image,
+            # define the text to be retrieved when the button is pressed
+            text="TRUE",
+            text_color=BACKGROUND,
+            fg_color="transparent",
+            bg_color=BACKGROUND,
+            hover_color=BACKGROUND,
+            # command=lambda: self.setup_labels(),
+            command=lambda: [
+                self.validate_answer(self.accept_button),
+                self.setup_labels(),
+            ],
+        )
+        self.accept_button.grid(row=1, column=0)
+        # print(self.accept_button.cget("text"))
+
+        # FALSE button
+        false_button_image = ctk.CTkImage(
+            light_image=Image.open(FALSE_BUTTON_IMAGE),
+            size=(80, 80),
+        )
+
+        self.refuse_button = ctk.CTkButton(
+            master=self.window,
+            image=false_button_image,
+            # define the text to be retrieved when the button is pressed
+            text="FALSE",
+            text_color=BACKGROUND,
+            fg_color="transparent",
+            bg_color=BACKGROUND,
+            hover_color=BACKGROUND,
+            # command=lambda: self.setup_labels(),
+            command=lambda: [
+                self.validate_answer(self.refuse_button),
+                self.setup_labels(),
+            ],
+        )
+        self.refuse_button.grid(row=1, column=1)
+
+    def pick_random_question(self, question_list: dict) -> str:
+        """Return a random question picked from the Open Trivia API.
+
+        Parameters
+        ----------
+        question_list : list
+            A list of questions taken from the Open Trivia API.
+
+        Returns
+        -------
+        str
+            One random question picked from the list.
+        """
+        return choice(list(question_list.items()))
+
+    def validate_answer(self, button):
+        """Validates the user's answer by comparing button text with the correct answer."""
+        user_answer = button.cget("text")  # Retrieve text from button
+        if user_answer == self.current_answer:
+            self.score_point += 1
+            print(
+                f"Correct! Current score is: {self.score_point}"
+            )  # Or handle correct answer (e.g., update score)
+        else:
+            print(
+                f"Incorrect! Current score is: {self.score_point}"
+            )  # Or handle incorrect answer (e.g., show feedback)
