6b7a48f24534a6919bbbfe10c84482ac73c1d9c1
alessandro.maccario <alessandro.maccario@proton.me>
2025-09-04 10:44:48 +0200

    initial commit: day_57 jinja in flask applications

---
+++ course_100_days_of_code/day_57/src/server.py
@@ -0,0 +1,21 @@
+"""
+Day 57: Templating with Jinja in Flask Applications
+
+Reference:
+- https://codepen.io/candytale55/pen/EaVExNX
+"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index_2.html")
+
+
+if __name__ == "__main__":
+    # run the app in debug mode to auto-reload the server
+    app.run(debug=True)
