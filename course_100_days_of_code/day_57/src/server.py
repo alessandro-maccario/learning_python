46b8abd8629e402a8aa0fcf4b82bebae9e626244
alessandro.maccario <alessandro.maccario@proton.me>
2025-09-05 23:07:01 +0200

    feat: complete the task by adding jinja template and variable to the html page

--- course_100_days_of_code/day_57/src/server.py
+++ course_100_days_of_code/day_57/src/server.py
@@ -3,18 +3,37 @@ Day 57: Templating with Jinja in Flask Applications
 
 Reference:
 - https://codepen.io/candytale55/pen/EaVExNX
+- https://genderize.io/documentation
 """
 
 from flask import Flask, render_template
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
+
+# define the current year to be used in the Copyright section
+current_year = datetime.now().year
 
 
 @app.route("/")
 def home():
-    current_year = datetime.now().year
     return render_template("index.html", current_year=current_year)
+
+
+@app.route("/guess/<name>")
+def landing_page(name: str):
+    # first letter of the name must be capitalized
+    capitalized_name = name.capitalize()
+    # return the name to be displayed and to be used to guess the gender
+    gender_name = requests.get(f"https://api.genderize.io?name={name}").json()["gender"]
+    # send the variable to jinja in the html page
+    return render_template(
+        "index_2.html",
+        name=capitalized_name,
+        gender_name=gender_name,
+        current_year=current_year,
+    )
 
 
 if __name__ == "__main__":
