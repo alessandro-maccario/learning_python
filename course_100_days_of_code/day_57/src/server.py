ef74f20d0188a732be5917481c462a6de1be744d
alessandro.maccario <alessandro.maccario@proton.me>
2025-09-06 21:50:16 +0200

    feat: add fake blog post to start with for loop and if statements in jinja

--- course_100_days_of_code/day_57/src/server.py
+++ course_100_days_of_code/day_57/src/server.py
@@ -34,6 +34,14 @@ def guess_name(name: str):
     )
 
 
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
+
 if __name__ == "__main__":
     # run the app in debug mode to auto-reload the server
     app.run(debug=True)
