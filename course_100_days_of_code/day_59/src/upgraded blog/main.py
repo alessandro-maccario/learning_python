ea850295eda3cc0fc0bd0ca7ed32659a10b2296a
alessandro.maccario <alessandro.maccario@proton.me>
2025-09-15 19:53:17 +0200

    feat: add about and contact html files and blog data in json format

--- course_100_days_of_code/day_59/src/upgraded blog/main.py
+++ course_100_days_of_code/day_59/src/upgraded blog/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,23 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+
+# Import the npoint.io json blog data
+blog_posts = requests.get(
+    "https://api.npoint.io/12dbc378eab2f22b5542", auth=("user", "pass")
+)
+print(blog_posts.json())
 
 
 if __name__ == "__main__":
