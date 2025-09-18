93d75f78226ff2977ebc2c77e1d9de1696878183
alessandro.maccario <alessandro.maccario@proton.me>
2025-09-18 17:52:01 +0200

    feat: add day_60 previous template to work on the contact form

--- course_100_days_of_code/day_60/src/html_form/main.py
+++ course_100_days_of_code/day_60/src/html_form/main.py
@@ -1,6 +1,10 @@
+"""
+CHALLENGE: Create a HTML Form in index.html so that when rendered as a webpage, this is what you see:
+"""
+
 from flask import Flask
 from flask import render_template
-import requests
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,6 +12,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["inputName"]
+        password = request.form["inputPassword"]
+        return f"<h1>Name: {username} | Password: {password}</h1>"
+    else:
+        return render_template("{{ url_for('login') }}")
 
 
 if __name__ == "__main__":
