90e5d52f02fa94d1ef99e503b979e618aad0271a
alessandro.maccario <alessandro.maccario@proton.me>
2025-09-21 08:30:16 +0200

    feat: create the WTF login page

--- course_100_days_of_code/day_61/src/main.py
+++ course_100_days_of_code/day_61/src/main.py
@@ -1,26 +1,31 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import DataRequired, InputRequired
+import os
+from dotenv import load_dotenv
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+
+class MyForm(FlaskForm):
+    userEmail = StringField("Email")
+    userPassword = StringField("Password")
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = MyForm()
+    return render_template("login.html", form=form)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
