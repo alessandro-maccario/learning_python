03c9b6b1c8b109487d557af793c723b606b1c1bd
alessandro.maccario <alessandro.maccario@proton.me>
2025-09-28 20:03:36 +0200

    feat: add edit and delete endpoint

--- course_100_days_of_code/day_63/src/main.py
+++ course_100_days_of_code/day_63/src/main.py
@@ -3,9 +3,9 @@ from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
-from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired, URL
+from sqlalchemy import String, Float, delete
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import os
 from dotenv import load_dotenv
 
@@ -57,13 +57,19 @@ class AddBook(FlaskForm):
     submit = SubmitField("Add a book")
 
 
+class EditRating(FlaskForm):
+    edit_rating = StringField("New Rating", validators=[DataRequired()])
+    submit = SubmitField("Edit Rating")
+
+
 @app.route("/")
 def home():
     # query the book table and select all the entries ordered by book title
     books = db.session.execute(db.select(Book).order_by(Book.title)).scalars()
-    return render_template("index.html", book_list=books)
+    return render_template("index.html", book_list=list(books))
 
 
+# Endpoint for adding a record
 @app.route("/add", methods=["GET", "POST"])
 def add():
     form = AddBook()
@@ -79,6 +85,37 @@ def add():
 
         return redirect(url_for("home"))
     return render_template("add.html", form=form)
+
+
+# Endpoint for editing a record
+@app.route("/edit/<int:book_id>", methods=["GET", "POST"])
+def edit(book_id):
+    form = EditRating()
+    if form.validate_on_submit():
+        # fetch the correct book id to be edited
+        book_to_update = db.session.execute(
+            db.select(Book).where(Book.id == book_id)
+        ).scalar_one()
+        # add the new rating to the same book to update
+        book_to_update.rating = request.form["edit_rating"]
+        # commit to update the previous rating
+        db.session.commit()
+
+        return redirect(url_for("home"))
+    return render_template("edit.html", form=form, book_id=book_id)
+
+
+# Endpoint for deleting a record
+@app.route("/delete/<int:book_id>", methods=["GET", "DELETE"])
+def delete(book_id):
+    # search for the book to remove from the db
+    book_to_delete = db.session.execute(
+        db.select(Book).where(Book.id == book_id)
+    ).scalar()
+    db.session.delete(book_to_delete)
+    # commit to update the previous rating
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
