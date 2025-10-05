3a91948263ab1d504759c2f611118cfa64431b0c
alessandro.maccario <alessandro.maccario@proton.me>
2025-10-05 11:33:56 +0200

    feat: add env variable

--- course_100_days_of_code/day_67/src/main.py
+++ course_100_days_of_code/day_67/src/main.py
@@ -8,28 +8,23 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
+import os
+from dotenv import load_dotenv
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
+load_dotenv()  # take environment variables
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 Bootstrap5(app)
+
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -49,14 +44,15 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/')
+@app.route("/")
 def get_all_posts():
     # TODO: Query the database for all the posts. Convert the data to a python list.
     posts = []
     return render_template("index.html", all_posts=posts)
 
+
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route("/")
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
     requested_post = "Grab the post from your database"
@@ -68,6 +64,7 @@ def show_post(post_id):
 # TODO: edit_post() to change an existing blog post
 
 # TODO: delete_post() to remove a blog post from the database
+
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
