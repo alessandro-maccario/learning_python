0495fd08825c3ad2ab70ec7f2526a19f16c5ffa1
alessandro.maccario <alessandro.maccario@proton.me>
2025-10-19 11:18:17 +0200

    docs: add instructions

--- day_74/main.py
+++ day_74/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from day-74!")
+# 1. Let's find out how many different colour LEGO bricks are actually in production.
+# Read the colors.csv file from the data folder and find the total number of unique colours.
+# There's a number of different ways you can accomplish this. Maybe try using the .nunique() from Pandas.
+import pandas as pd
 
-
-if __name__ == "__main__":
-    main()
+# 2. Figure out how many of the LEGO colours are transparent compared to how many colours are opaque.
+# See if you can Google your way to finding at least two different ways of arriving at the answer.
