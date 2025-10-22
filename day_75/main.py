ba5cc224d062f4c37680d036f8acc079cc37a176
alessandro.maccario <alessandro.maccario@proton.me>
2025-10-22 17:30:48 +0200

    feat: add unemployment search and trend plot

--- day_75/main.py
+++ day_75/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import datetime
 
 # read the datasets in
 df_tesla = pd.read_csv(
@@ -111,10 +112,38 @@ years_fmt = mdates.DateFormatter("%Y")
 # plt.show()
 
 
-# 8. Plot: Bitcoin
+# # 8. Plot: Bitcoin
+# # define the figure boundaries
+# plt.figure(figsize=(14, 8))
+# plt.title("Bitcoin News Search vs Resampled Price", fontsize=18)
+
+# # Increase the size and rotate the labels on the x-axis
+# plt.xticks(fontsize=14, rotation=45)
+
+# ax1 = plt.gca()  # get current axis
+# ax2 = ax1.twinx()
+
+# # Also, increase fontsize and linewidth for larger charts
+# ax1.set_ylabel("BTC Price", color="#E6232E", fontsize=14)
+# ax2.set_ylabel("Search Trend", color="skyblue", fontsize=14)
+
+# # format the ticks on the axis with Locators
+# ax1.xaxis.set_major_locator(years)
+# ax1.xaxis.set_major_formatter(years_fmt)
+# ax1.xaxis.set_minor_locator(months)
+
+# # Displays chart explicitly
+# ax1.plot(df_monthly_bitcoin.MONTH, df_monthly_bitcoin.CLOSE, color="#FD9A9F")
+# ax2.plot(df_bitcoin_trend.MONTH, df_bitcoin_trend.BTC_NEWS_SEARCH, color="skyblue")
+# plt.show()
+
+
+# 9. Plot: Unemployment
 # define the figure boundaries
 plt.figure(figsize=(14, 8))
-plt.title("Bitcoin News Search vs Resampled Price", fontsize=18)
+plt.title(
+    "Monthly Search of 'Unemployment Benefits' in the U.S. vs the U/E Rate", fontsize=18
+)
 
 # Increase the size and rotate the labels on the x-axis
 plt.xticks(fontsize=14, rotation=45)
@@ -123,19 +152,22 @@ ax1 = plt.gca()  # get current axis
 ax2 = ax1.twinx()
 
 # Also, increase fontsize and linewidth for larger charts
-ax1.set_ylabel("BTC Price", color="#E6232E", fontsize=14)
+ax1.set_ylabel("FRED U/E Rate", color="#E6232E", fontsize=14)
 ax2.set_ylabel("Search Trend", color="skyblue", fontsize=14)
-
-# Set the minimum and maximum values on the axes
-# ax1.set_ylim([0, 600])
-# ax1.set_xlim([df_tesla.MONTH.min(), df_tesla.MONTH.max()])
 
 # format the ticks on the axis with Locators
 ax1.xaxis.set_major_locator(years)
 ax1.xaxis.set_major_formatter(years_fmt)
 ax1.xaxis.set_minor_locator(months)
+ax1.grid(color="grey", linestyle="--")
 
+# filter only for data until 2019
+df_unemployment = df_unemployment[df_unemployment["MONTH"] < "2019-12-31"]
+# Calculate the rolling average over a 6 month window
+roll_df = df_unemployment[["UE_BENEFITS_WEB_SEARCH", "UNRATE"]].rolling(window=6).mean()
 # Displays chart explicitly
-ax1.plot(df_monthly_bitcoin.MONTH, df_monthly_bitcoin.CLOSE, color="#FD9A9F")
-ax2.plot(df_bitcoin_trend.MONTH, df_bitcoin_trend.BTC_NEWS_SEARCH, color="skyblue")
+ax1.plot(df_unemployment.MONTH, roll_df.UNRATE, color="#FD9A9F")
+ax2.plot(df_unemployment.MONTH, roll_df.UE_BENEFITS_WEB_SEARCH, color="skyblue")
 plt.show()
+
+# print((df_unemployment[df_unemployment["MONTH"] < "2019-07-01"]))
