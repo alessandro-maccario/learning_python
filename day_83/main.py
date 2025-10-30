741038dd100abc118f4dd05cec812a39a1597d86
alessandro.maccario <alessandro.maccario@proton.me>
2025-10-30 21:14:33 +0100

    feat: add flask routes to the html templates

--- day_83/main.py
+++ day_83/main.py
@@ -9,5 +9,35 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/experience")
+def experience_page():
+    return render_template("experience.html")
+
+
+@app.route("/education")
+def education_page():
+    return render_template("education.html")
+
+
+@app.route("/skills")
+def skills_page():
+    return render_template("skills.html")
+
+
+@app.route("/interests")
+def interests_page():
+    return render_template("interests.html")
+
+
+@app.route("/projects")
+def projects_page():
+    return render_template("projects.html")
+
+
+@app.route("/certifications")
+def certifications_page():
+    return render_template("certifications.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
