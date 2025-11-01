b90df33dca4fe1ca8088c920109e9f23f6626cbb
alessandro.maccario <alessandro.maccario@proton.me>
2025-11-01 19:48:13 +0100

    feat: add blog and now routes

--- day_83/main.py
+++ day_83/main.py
@@ -25,11 +25,6 @@ def skills_page():
     return render_template("skills.html")
 
 
-@app.route("/interests")
-def interests_page():
-    return render_template("interests.html")
-
-
 @app.route("/projects")
 def projects_page():
     with open("instance/projects.json") as json_file:
@@ -41,6 +36,19 @@ def projects_page():
 @app.route("/certifications")
 def certifications_page():
     return render_template("certifications.html")
+
+
+@app.route("/blog")
+def blog_page():
+    return render_template("blog.html")
+
+
+@app.route("/now")
+def now_page():
+    with open("instance/books.json") as json_file:
+        books_json_data = json.load(json_file)
+
+    return render_template("now.html", books=books_json_data)
 
 
 if __name__ == "__main__":
