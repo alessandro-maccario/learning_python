d5b8c4d318ba9cdda4a9fc1918c0cf7d6146c991
alessandro.maccario <alessandro.maccario@proton.me>
2025-11-04 18:50:39 +0100

    feat: add add_task and delete_task routes

--- day_89/app/routes.py
+++ day_89/app/routes.py
@@ -1,34 +1,24 @@
 from app import app
-from flask import render_template, request, jsonify
-from app.models import db, Cafe
+from flask import render_template, request, redirect, url_for
+from app.models import tasks
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", tasks=tasks)
 
 
-# HTTP GET - Read Record
-@app.route("/all", methods=["GET"])
-def fetch_all_cafe():
-    if request.method == "GET":
-        select_all_cafe = db.session.execute(db.select(Cafe)).scalars().all()
-        dict_all_cafe = {
-            "cafes": [
-                {
-                    "name": cafe.name,
-                    "map_url": cafe.map_url,
-                    "img_url": cafe.img_url,
-                    "location": cafe.location,
-                    "seats": cafe.seats,
-                    "has_toilet": cafe.has_toilet,
-                    "has_wifi": cafe.has_wifi,
-                    "has_sockets": cafe.has_sockets,
-                    "can_take_calls": cafe.can_take_calls,
-                    "coffee_price": cafe.coffee_price,
-                }
-                for cafe in select_all_cafe
-            ]
-        }
+@app.route("/add", methods=["POST"])
+def add_task():
+    new_task = request.form.get("todoitem")
+    if new_task:
+        tasks.append(new_task)
+    return redirect(url_for("index"))
 
-        return render_template("cafes.html", cafes_data=dict_all_cafe)
+
+# Add a new route to handle task deletion
+@app.route("/delete/<int:id>", methods=["GET", "POST"])
+def delete_task(id):
+    # remove the task from the list
+    tasks.pop(id - 1)
+    return redirect(url_for("index"))
